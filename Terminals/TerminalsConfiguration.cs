85c06cf0b831211ce34196b432fb283eef182fda
dudushmaya_cp <SND\dudushmaya_cp>
2006-11-01 21:13:52 +0000

    Removed unused sources.

--- Terminals/TerminalsConfiguration.cs
+++ Terminals/TerminalsConfiguration.cs
@@ -5,6 +5,9 @@ using System.Configuration;
 
 namespace Terminals
 {
+    public enum DesktopSize { x640 = 0, x800, x1024, FitToWindow, FullScreen, AutoScale };
+    public enum Colors { Bits8 = 0, Bit16, Bits24 };
+
     public class TerminalsConfigurationSection : ConfigurationSection
     {
         public TerminalsConfigurationSection()
