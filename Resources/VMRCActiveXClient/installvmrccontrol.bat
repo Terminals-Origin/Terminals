a546bb7f190787527cfe51700f4af08d6be25aab
RobChartier_cp <SND\RobChartier_cp>
2007-04-06 09:00:04 +0000

    Fixed the batch file which installs the VMRC ActiveX Client.

--- Resources/VMRCActiveXClient/installvmrccontrol.bat
+++ Resources/VMRCActiveXClient/installvmrccontrol.bat
@@ -1,2 +1,5 @@
-xcopy *.* "%systemroot%\" /t /h 
+echo n | xcopy VMKeyboardHook.dll "%systemroot%\"
+echo n | xcopy VMRCActiveXClient.dll "%systemroot%\"
+echo n | xcopy VMRCActiveXClient.inf "%systemroot%\"
 regsvr32 /s "%systemroot%\VMRCActiveXClient.dll"
+pause
