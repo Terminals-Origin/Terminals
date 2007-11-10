bf37545bec61a142b98cc1d1c760c94be7e08fd3
RobChartier_cp <SND\RobChartier_cp>
2007-11-10 03:35:32 +0000

    Network shares can now list shares on any remote machine, given appropriate credentials

--- Terminals/Network/WMI/NetworkShares.Designer.cs
+++ Terminals/Network/WMI/NetworkShares.Designer.cs
@@ -29,6 +29,8 @@ namespace Terminals.Network
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.wmiServerCredentials1 = new Terminals.Network.WMI.WMIServerCredentials();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,17 +40,40 @@ namespace Terminals.Network
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 83);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(399, 218);
+            this.dataGridView1.Size = new System.Drawing.Size(399, 135);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // wmiServerCredentials1
+            // 
+            this.wmiServerCredentials1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.wmiServerCredentials1.Location = new System.Drawing.Point(0, 0);
+            this.wmiServerCredentials1.Name = "wmiServerCredentials1";
+            this.wmiServerCredentials1.Password = "Rob";
+            this.wmiServerCredentials1.SelectedServer = "";
+            this.wmiServerCredentials1.Size = new System.Drawing.Size(399, 83);
+            this.wmiServerCredentials1.TabIndex = 1;
+            this.wmiServerCredentials1.Username = "\\APOLLO\\Rob";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // NetworkShares
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.wmiServerCredentials1);
             this.Name = "NetworkShares";
             this.Size = new System.Drawing.Size(399, 218);
             this.Load += new System.EventHandler(this.NetworkShares_Load);
@@ -60,5 +85,7 @@ namespace Terminals.Network
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private Terminals.Network.WMI.WMIServerCredentials wmiServerCredentials1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Terminals/Network/WMI/NetworkShares.cs
+++ Terminals/Network/WMI/NetworkShares.cs
@@ -14,51 +14,59 @@ namespace Terminals.Network
         {
             InitializeComponent();
         }
-        
-        private void NetworkShares_Load(object sender, EventArgs e)
-        {
+        private void LoadShares(string Username, string Password, string Computer) {
             List<Share> shares = new List<Share>();
             System.Text.StringBuilder sb = new StringBuilder();
             string qry = "select * from win32_share";
             System.Management.ManagementObjectSearcher searcher;
             System.Management.ObjectQuery query = new System.Management.ObjectQuery(qry);
-            searcher = new System.Management.ManagementObjectSearcher(query);
-            foreach (System.Management.ManagementObject share in searcher.Get())
-            {
+
+            if(Username != "" && Password != "" && Computer != "" && !Computer.StartsWith(@"\\localhost")) {
+                System.Management.ConnectionOptions oConn = new System.Management.ConnectionOptions();
+                oConn.Username = Username;
+                oConn.Password = Password;
+                if(!Computer.StartsWith(@"\\")) Computer = @"\\" + Computer;
+                if(!Computer.ToLower().EndsWith(@"\root\cimv2")) Computer = Computer + @"\root\cimv2";
+                System.Management.ManagementScope oMs = new System.Management.ManagementScope(Computer, oConn);
+
+                searcher = new System.Management.ManagementObjectSearcher(oMs, query);
+            } else {
+                searcher = new System.Management.ManagementObjectSearcher(query);
+            }
+
+            foreach(System.Management.ManagementObject share in searcher.Get()) {
                 Share s = new Share();
-                foreach (System.Management.PropertyData p in share.Properties)
-                {
-                    switch (p.Name)
-                    {
+                foreach(System.Management.PropertyData p in share.Properties) {
+                    switch(p.Name) {
                         case "AccessMask":
-                            if(p.Value!=null) s.AccessMask = p.Value.ToString();
+                            if(p.Value != null) s.AccessMask = p.Value.ToString();
                             break;
                         case "MaximumAllowed":
-                            if (p.Value != null) s.MaximumAllowed = p.Value.ToString();
+                            if(p.Value != null) s.MaximumAllowed = p.Value.ToString();
                             break;
                         case "InstallDate":
-                            if (p.Value != null) s.InstallDate = p.Value.ToString();
+                            if(p.Value != null) s.InstallDate = p.Value.ToString();
                             break;
                         case "Description":
-                            if (p.Value != null) s.Description = p.Value.ToString();
+                            if(p.Value != null) s.Description = p.Value.ToString();
                             break;
                         case "Caption":
-                            if (p.Value != null) s.Caption = p.Value.ToString();
+                            if(p.Value != null) s.Caption = p.Value.ToString();
                             break;
                         case "AllowMaximum":
-                            if (p.Value != null) s.AllowMaximum = p.Value.ToString();
+                            if(p.Value != null) s.AllowMaximum = p.Value.ToString();
                             break;
                         case "Name":
-                            if (p.Value != null) s.Name = p.Value.ToString();
+                            if(p.Value != null) s.Name = p.Value.ToString();
                             break;
                         case "Path":
-                            if (p.Value != null) s.Path = p.Value.ToString();
+                            if(p.Value != null) s.Path = p.Value.ToString();
                             break;
                         case "Status":
-                            if (p.Value != null) s.Status = p.Value.ToString();
+                            if(p.Value != null) s.Status = p.Value.ToString();
                             break;
                         case "Type":
-                            if (p.Value != null) s._Type = p.Value.ToString();
+                            if(p.Value != null) s._Type = p.Value.ToString();
                             break;
                         default:
                             break;
@@ -67,6 +75,16 @@ namespace Terminals.Network
                 shares.Add(s);
             }
             this.dataGridView1.DataSource = shares;
+        }
+        private void NetworkShares_Load(object sender, EventArgs e)
+        {
+
+            this.LoadShares("", "", "");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            this.LoadShares(this.wmiServerCredentials1.Username, this.wmiServerCredentials1.Password, this.wmiServerCredentials1.SelectedServer);
         }
     }
     public class Share
