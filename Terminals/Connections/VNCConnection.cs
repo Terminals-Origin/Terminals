30a55fc0d809ca951c483469835c8e7e37da60e4
RobChartier_cp <SND\RobChartier_cp>
2008-04-20 17:53:18 +0000

    attempting to resolve some issues around VNC connection exceptions during paint on disconnecting

--- Terminals/Connections/VNCConnection.cs
+++ Terminals/Connections/VNCConnection.cs
@@ -3,51 +3,38 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Terminals.Connections
-{
-    public class VNCConnection : Connection
-    {
+namespace Terminals.Connections {
+    public class VNCConnection : Connection {
         #region IConnection Members
         private bool connected = false;
-        public override void ChangeDesktopSize(Terminals.DesktopSize Size)
-        {
+        public override void ChangeDesktopSize(Terminals.DesktopSize Size) {
         }
 
-        public void SendSpecialKeys(VncSharp.SpecialKeys Keys)
-        {
+        public void SendSpecialKeys(VncSharp.SpecialKeys Keys) {
 
             rd.SendSpecialKeys(Keys);
         }
         public override bool Connected { get { return connected; } }
         VncSharp.RemoteDesktop rd;
-        public override bool Connect()
-        {
-            try
-            {
+        public override bool Connect() {
+            try {
                 rd = new VncSharp.RemoteDesktop();
                 Controls.Add(rd);
 
                 string pass = Favorite.Password;
                 if(pass == null || pass == "") pass = Settings.DefaultPassword;
-                
+
                 this.vncPassword = pass;
 
-                if(pass == null || pass == "")
-                {
+                if(pass == null || pass == "") {
                     Terminals.InputBoxResult result = Terminals.InputBox.Show("VNC Password", "Please specify your password now", '*');
-                    if(result.ReturnCode == DialogResult.OK)
-                    {
-                        if(result.Text != null && result.Text != "")
-                        {
+                    if(result.ReturnCode == DialogResult.OK) {
+                        if(result.Text != null && result.Text != "") {
                             this.vncPassword = result.Text;
-                        }
-                        else
-                        {
+                        } else {
                             return false;
                         }
-                    }
-                    else
-                    {
+                    } else {
                         return false;
                     }
                 }
@@ -56,7 +43,7 @@ namespace Terminals.Connections
                 this.Parent = TerminalTabPage;
                 rd.Dock = DockStyle.Fill;
                 rd.VncPort = Favorite.Port;
-                
+
 
                 rd.ConnectComplete += new VncSharp.ConnectCompleteHandler(rd_ConnectComplete);
                 rd.GetPassword = VNCPassword;
@@ -65,43 +52,38 @@ namespace Terminals.Connections
                 rd.BringToFront();
                 return true;
 
-            }
-            catch(Exception exc)
-            {
+            } catch(Exception exc) {
                 Terminals.Logging.Log.Fatal("Connecting to VNC", exc);
                 return false;
             }
         }
         private string vncPassword = "";
-        string VNCPassword()
-        {
+        string VNCPassword() {
             return vncPassword;
         }
 
-        void rd_ConnectComplete(object sender, VncSharp.ConnectEventArgs e)
-        {
+        void rd_ConnectComplete(object sender, VncSharp.ConnectEventArgs e) {
             // Update Form to match geometry of remote desktop
             //ClientSize = new Size(e.DesktopWidth, e.DesktopHeight);
-
-            connected = true;
-            VncSharp.RemoteDesktop rd = (VncSharp.RemoteDesktop)sender;
-            rd.Visible = true;
-            rd.BringToFront();
-            rd.FullScreenUpdate();
-            rd.Enabled = true;
+            try {
+                connected = true;
+                VncSharp.RemoteDesktop rd = (VncSharp.RemoteDesktop)sender;
+                rd.Visible = true;
+                rd.BringToFront();
+                rd.FullScreenUpdate();
+                rd.Enabled = true;
+            } catch(Exception Exc) {
+                Terminals.Logging.Log.Fatal("ConnectComplete to VNC", Exc);
+            }
             // Change the Form's title to match desktop name
         }
 
-        public override void Disconnect()
-        {
-            try
-            {
+        public override void Disconnect() {
+            try {
                 connected = false;
                 rd.Disconnect();
-            }
-            catch(Exception e)
-            {
-                Terminals.Logging.Log.Info("", e);
+            } catch(Exception e) {
+                Terminals.Logging.Log.Info("Disconnect", e);
             }
         }
 
