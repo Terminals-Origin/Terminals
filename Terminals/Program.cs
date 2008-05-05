b9e4629278900dba5051c24b69c7231cb7c8125b
RobChartier_cp <SND\RobChartier_cp>
2008-05-05 19:03:04 +0000

    Release 1.6k. Updated build number for release.

--- Terminals/Program.cs
+++ Terminals/Program.cs
@@ -9,9 +9,9 @@ namespace Terminals
 {
     static class Program
     {
-        public static string TerminalsVersion = "1.6j";
+        public static string TerminalsVersion = "1.6k";
         //  reminder to update the buildate for each release
-        public static DateTime BuildDate = new DateTime(2008, 4, 30);  //used for checking project releases.  yeah yeah, this could be smarter about things...
+        public static DateTime BuildDate = new DateTime(2008, 5, 5);  //used for checking project releases.  yeah yeah, this could be smarter about things...
         public static Mutex mtx;
         /// <summary>
         /// The main entry point for the application.
