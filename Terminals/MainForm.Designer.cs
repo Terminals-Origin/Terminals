bc34c5bad4079c8e7650085029374935608f6b4d
RobChartier_cp <SND\RobChartier_cp>
2009-10-03 04:51:42 +0000

    Added GodKratos fix for the left favorite bar

--- Terminals/MainForm.Designer.cs
+++ Terminals/MainForm.Designer.cs
@@ -834,6 +834,7 @@ namespace Terminals {
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
             // 
