99c3c86a4e2dbe000e8ada8a842ac5f4f37446ab
ModeenF_cp <SND\ModeenF_cp>
2010-05-12 07:01:36 +0000

    We don't want today’s date, we want the date of the build (that was the intention anyway), and this will get the date of the exe file.

--- Terminals/Program.cs
+++ Terminals/Program.cs
@@ -11,8 +11,9 @@ namespace Terminals
 {
     static class Program
     {
+        
         public static string TerminalsVersion = "1.9a";
-        public static DateTime BuildDate = DateTime.Now;
+        public static DateTime BuildDate = RetrieveLinkerTimestamp();
         public static Assembly aAssembly = Assembly.GetExecutingAssembly();
         public static AssemblyDescriptionAttribute desc = (AssemblyDescriptionAttribute)AssemblyDescriptionAttribute.GetCustomAttribute(aAssembly, typeof(AssemblyDescriptionAttribute));
         public static AssemblyTitleAttribute title = (AssemblyTitleAttribute)AssemblyTitleAttribute.GetCustomAttribute(aAssembly, typeof(AssemblyTitleAttribute));
@@ -100,5 +101,38 @@ namespace Terminals
                 Terminals.Program.ConfigurationFileLocation = Terminals.MainForm.CommandLineArgs.config;
             }
         }
+
+        /// <summary>
+        /// Taken from http://stackoverflow.com/questions/1600962/c-displaying-the-build-date
+        /// (code by Joe Spivey)
+        /// </summary>
+        /// <returns></returns>
+        private static DateTime RetrieveLinkerTimestamp()
+        {
+            string filePath = System.Reflection.Assembly.GetCallingAssembly().Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+            byte[] b = new byte[2048];
+            System.IO.Stream s = null;
+
+            try
+            {
+                s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                s.Read(b, 0, 2048);
+            }
+            finally
+            {
+                if (s != null)
+                    s.Close();
+            }
+
+            int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+            int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+            dt = dt.AddSeconds(secondsSince1970);
+            dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+            return dt;
+        } 
+
     }
 }
