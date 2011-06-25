066d09c2d7a303a7480a67d4dee55c44ddd386e9
RobChartier_cp <SND\RobChartier_cp>
2011-06-25 23:42:44 +0000

    Added basic navigation controls to the HTTP Connection

--- Terminals/Connections/HTTPConnection.cs
+++ Terminals/Connections/HTTPConnection.cs
@@ -4,9 +4,10 @@ using System.Text;
 
 namespace Terminals.Connections
 {
-    public class HTTPConnection : Connection//System.Windows.Forms.Control
+    public class HTTPConnection : Connection
     {
-    
+
+        MiniBrowser browser = new MiniBrowser();
         public override bool Connected
         {
             get { return true; }
@@ -19,20 +20,10 @@ namespace Terminals.Connections
         private void EnsureConnection()
         {
         }
-        System.Windows.Forms.WebBrowser webBrowser1;
         public override bool Connect()
         {
             try
             {
-                webBrowser1 = new System.Windows.Forms.WebBrowser();
-
-                this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-                this.webBrowser1.Location = new System.Drawing.Point(0, 0);
-                this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-                this.webBrowser1.Name = "webBrowser1";
-                this.webBrowser1.Size = new System.Drawing.Size(292, 273);
-                this.webBrowser1.TabIndex = 0;
-                
                 this.Dock = System.Windows.Forms.DockStyle.Fill;
 
 
@@ -45,19 +36,22 @@ namespace Terminals.Connections
                 if (string.IsNullOrEmpty(userName)) userName = Settings.DefaultUsername;
 
 
+                this.browser.Home = Favorite.Url;
 
                 if(!String.IsNullOrEmpty(Favorite.UserName) && !String.IsNullOrEmpty(Favorite.Password)) {
                     string hdr = "Authorization: Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(userName + ":" + pass)) + System.Environment.NewLine;
-                    this.webBrowser1.Navigate(Favorite.Url,null,null, hdr);
+                    this.browser.Browser.Navigate(Favorite.Url,null,null, hdr);
                 } else {
-                    this.webBrowser1.Navigate(Favorite.Url);
+                    this.browser.Browser.Navigate(Favorite.Url);
                 }
+                
 
-                this.Controls.Add(this.webBrowser1);
-                this.webBrowser1.Parent = this;
+                this.Controls.Add(this.browser);
+                this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+                this.browser.Parent = this;
                 this.Parent = TerminalTabPage;
                 this.BringToFront();
-                this.webBrowser1.BringToFront();
+                this.browser.BringToFront();
                 
             }
             catch(Exception exc)
