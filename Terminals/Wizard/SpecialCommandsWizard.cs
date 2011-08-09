9bc5a7245c5d19a4fb40ebc6ac4b732e55917d06
jirkapok_cp <SND\jirkapok_cp>
2011-08-09 23:18:14 +0000

    Control panel applets now have better name for well known applets

--- Terminals/Wizard/SpecialCommandsWizard.cs
+++ Terminals/Wizard/SpecialCommandsWizard.cs
@@ -14,7 +14,48 @@ namespace Terminals.Wizard
 
         private const string THUMBS_DIRECTORY = "Thumbs";
 
-        private static SortedDictionary<string, string> KnownSpecialCommands = new SortedDictionary<string, string>();
+        /// <summary>
+        /// Because it isn't easy to obtain the name of an applet.
+        /// This holds atleast known applet names in a form of key=applet_executable, value=applet_name 
+        /// </summary>
+        private static readonly SortedDictionary<string, string> knownSystemControlApplets = new SortedDictionary<string, string>
+            {
+             {"access.cpl", "Accesibility Options"},
+             {"appwiz.cpl", "Add or Remove programs"},
+             {"bthprops.cpl", "Bluetooth Devices"},
+             {"desk.cpl", "Display properties"},
+             {"firewall.cpl", "Windows Firewall"},
+             {"hdwwiz.cpl", "Add hardware wizard"},
+             {"inetcpl.cpl", "Internet properties"},
+             {"infocardcpl.cpl", "Info card"},
+             {"intl.cpl", "Regional and language options"},
+             {"irprops.cpl", "IR options"},
+             {"joy.cpl", "Game controllers"},
+             {"main.cpl", "Mause properties"},
+             {"mmsys.cpl", "Sounds and audio devices"},
+             {"ncpa.cpl", "Network connections"},
+             {"netsetup.cpl", "Wireless network setup wizard"},
+             {"nusrmgr.cpl", "User accounts"},
+             {"powercfg.cpl", "Power options properties"},
+             {"sysdm.cpl", "System properties"},
+             {"telephon.cpl", "Phone and Modem Options"},
+             {"timedate.cpl", "Date and time properties"},
+             {"wscui.cpl", "Windows security center"},
+             {"wuaucpl.cpl", "Automatic updates"},
+             {"mlcfg32.cpl", "Mail"},
+             {"mlcfg.cpl","Mail"},
+             {"Sapi.cpl", "Speech"},
+             
+             // thirt party
+             {"igfxcpl.cpl", "Intel graphics media"},
+             {"javacpl.cpl", "Java"},
+             {"bdeadmin.cpl", "Borland database configuration"},
+             {"nwc.cpl", "Client Service for NetWare (CSNW)"},
+             {"odbccp32.cpl", "ODBC datasource administrator"},
+             {"RTSndMgr.CPL", "Realtec sound options"},
+             {"color.cpl", "Colors"},
+             {"FlashPlayerCPLApp.cpl", "Flash Player"}
+            };
 
         private static DirectoryInfo SystemRoot
         {
@@ -52,19 +93,28 @@ namespace Terminals.Wizard
                 Icon[] fileIcons = IconHandler.IconHandler.IconsFromFile(file.FullName, IconHandler.IconSize.Small);
                 if (fileIcons != null && fileIcons.Length > 0)
                 {
-                    string t = string.Format(THUMBS_FILENAME, file.Name);
-                    if (!File.Exists(t)) fileIcons[0].ToBitmap().Save(t);
-                    elm1.Thumbnail = t;
+                    string thumbnailIco = string.Format(THUMBS_FILENAME, file.Name);
+                    if (!File.Exists(thumbnailIco))
+                        fileIcons[0].ToBitmap().Save(thumbnailIco);
+                    elm1.Thumbnail = thumbnailIco;
                 }
 
                 string thumbName = string.Format(THUMBS_FILENAME, file.Name);
                 if (File.Exists(thumbName))
                     elm1.Thumbnail = thumbName;
 
-                elm1.Name = file.Name.Replace(file.Extension, "");
+                elm1.Name = EnsureAppletName(file);
                 elm1.Executable = @"%systemroot%\system32\" + file.Name;
                 cmdList.Add(elm1);
             }
+        }
+
+        private static string EnsureAppletName(FileInfo appletFile)
+        {
+            if (knownSystemControlApplets.ContainsKey(appletFile.Name))
+                return knownSystemControlApplets[appletFile.Name];
+
+            return appletFile.Name.Replace(appletFile.Extension, "");
         }
 
         private static void AddMmcCommands(SpecialCommandConfigurationElementCollection cmdList)
