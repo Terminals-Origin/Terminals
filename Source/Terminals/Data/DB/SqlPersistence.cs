b41fe1ca9f7bd5bd8496fa8783fe596b30a0fbad
JirkaPok <jirkapok@users.noreply.github.com>
2012-05-27 01:12:26 +0000

    Removed detach of Untaged group, wich is already not relevant. Moved prepared update of database persistence from database into sql persistence.

--- Source/Terminals/Data/DB/SqlPersistence.cs
+++ Source/Terminals/Data/DB/SqlPersistence.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Data.Objects;
+using System.Diagnostics;
+using System.Timers;
 
 namespace Terminals.Data.DB
 {
@@ -8,6 +11,11 @@ namespace Terminals.Data.DB
     /// </summary>
     internal class SqlPersistence : IPersistence, IPersistedSecurity
     {
+        /// <summary>
+        /// periodicaly download latest changes
+        /// </summary>
+        private Timer reLoadClock;
+
         private Database database;
 
         private Favorites favorites;
@@ -38,19 +46,14 @@ namespace Terminals.Data.DB
             this.Security.AssignPersistence(this);
         }
 
-        public void AssignSynchronizationObject(ISynchronizeInvoke synchronizer)
-        {
-          // todo this.database.InitializeReLoadClock(synchronizer);
-        }
-
         public void StartDelayedUpdate()
         {
-            this.database.StartDelayedUpdate();
+            // nothing to do here, database context trancks changes for us
         }
 
         public void SaveAndFinishDelayedUpdate()
         {
-            this.database.SaveAndFinishDelayedUpdate();
+            this.database.SaveImmediatelyIfRequested();
         }
 
         public void Initialize()
@@ -85,6 +88,64 @@ namespace Terminals.Data.DB
         public void UpdatePasswordsByNewMasterPassword(string newMasterPassword)
         {
             // nothing to do here, the application master password doesnt affect the database
+        }
+
+        public void AssignSynchronizationObject(ISynchronizeInvoke synchronizer)
+        {
+            this.reLoadClock = new Timer();
+            this.reLoadClock.Interval = 2000;
+            // this forces the clock to run the updates in gui thread, because Entity framework isnt thread safe
+            this.reLoadClock.SynchronizingObject = synchronizer;
+            this.reLoadClock.Elapsed += new ElapsedEventHandler(this.OnReLoadClockElapsed);
+            this.reLoadClock.Start();
+        }
+
+        /// <summary>
+        /// Wery simple refresh of all already cached items.
+        /// </summary>
+        private void OnReLoadClockElapsed(object sender, ElapsedEventArgs e)
+        {
+            this.reLoadClock.Stop();
+            // todo check, if there is atleast something new, otherwise we dont have to download everything
+            var clock = Stopwatch.StartNew();
+            this.RefreshDatabaseMasterPassword();
+            RefreshGroups();
+            RefreshFavorites();
+            RefreshSecurity();
+            RefreshHistory();
+            Debug.WriteLine("Updating entities at {0} [{1} ms]", DateTime.Now, clock.ElapsedMilliseconds);
+            this.reLoadClock.Start();
+        }
+
+        private void RefreshDatabaseMasterPassword()
+        {
+            // first download the masterpassword to ensure, that it wasnt changed,
+            // otherwise we have all cached items out of date
+        }
+
+        private void RefreshHistory()
+        {
+            // possible changes in history are only for today
+        }
+
+        private void RefreshSecurity()
+        {
+            //this.Refresh(RefreshMode.ClientWins, this.Security);
+        }
+
+        private void RefreshGroups()
+        {
+            //this.Refresh(RefreshMode.ClientWins, this.Groups);
+        }
+        
+        private void RefreshFavorites()
+        {
+            //this.Refresh(RefreshMode.ClientWins, this.Favorites);
+            //this.Refresh(RefreshMode.ClientWins, this.CredentialBase);
+            //this.Refresh(RefreshMode.ClientWins, this.BeforeConnectExecute);
+            //this.Refresh(RefreshMode.ClientWins, this.DisplayOptions);
+
+            // after all items are loaded, refresh already cached protocol options, which arent part of an entity
         }
     }
 }
