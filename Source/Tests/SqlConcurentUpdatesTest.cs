dfa4332da2bb61aa4da56f9eaa8199781ea2e4c4
JirkaPok <jirkapok@users.noreply.github.com>
2012-09-25 21:43:13 +0000

    Added new test in SQL tests to check database password updates.

--- Source/Tests/SqlConcurentUpdatesTest.cs
+++ Source/Tests/SqlConcurentUpdatesTest.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terminals.Configuration;
 using Terminals.Data;
+using Terminals.Data.DB;
+using Terminals.Security;
 using Favorite = Terminals.Data.DB.Favorite;
 
 namespace Tests
@@ -34,6 +38,9 @@ namespace Tests
         [TestCleanup]
         public void TestClose()
         {
+            Settings.DatabaseMasterPassword = string.Empty;
+            Database.UpdateMastrerPassord(string.Empty);
+
             // do not clean up the store here, because of waiting for background threads
             // this.lab.ClearTestLab();
         }
@@ -43,7 +50,7 @@ namespace Tests
         {
             this.AddFavoriteToPrimaryPersistence();
             this.AddFavoriteToPrimaryPersistence();
-            
+
             // assign event handler before another changes to catch all of them
             this.PrimaryPersistence.Dispatcher.FavoritesChanged +=
                 new FavoritesChangedEventHandler(this.OnPrimaryStoreFavoritesChanged);
@@ -61,25 +68,25 @@ namespace Tests
             Assert.AreEqual(TEST_NAME, this.updatedFavorite.Name, "The updated favorite wasnt refreshed");
         }
 
-      private void MakeChangesOnSecondaryPersistence()
-      {
-        List<IFavorite> secondary = this.SecondaryFavorites.ToList();
-        this.SecondaryFavorites.Delete(secondary[0]);
-        var favoriteA = secondary[1] as Favorite;
-        favoriteA.Name = TEST_NAME;
-        this.SecondaryFavorites.Update(favoriteA);
+        private void MakeChangesOnSecondaryPersistence()
+        {
+            List<IFavorite> secondary = this.SecondaryFavorites.ToList();
+            this.SecondaryFavorites.Delete(secondary[0]);
+            var favoriteA = secondary[1] as Favorite;
+            favoriteA.Name = TEST_NAME;
+            this.SecondaryFavorites.Update(favoriteA);
 
-        IFavorite favoriteB = this.SecondaryPersistence.Factory.CreateFavorite();
-        favoriteB.Name = "test";
-        favoriteB.ServerName = "test server";
-        this.SecondaryFavorites.Add(favoriteB);
-      }
+            IFavorite favoriteB = this.SecondaryPersistence.Factory.CreateFavorite();
+            favoriteB.Name = "test";
+            favoriteB.ServerName = "test server";
+            this.SecondaryFavorites.Add(favoriteB);
+        }
 
-      private void OnPrimaryStoreFavoritesChanged(FavoritesChangedEventArgs args)
+        private void OnPrimaryStoreFavoritesChanged(FavoritesChangedEventArgs args)
         {
             var updated = args.Updated.FirstOrDefault();
-            if (updated !=null)
-              this.updatedFavorite = updated;
+            if (updated != null)
+                this.updatedFavorite = updated;
 
             this.addEventCatched |= args.Added.Count > 0;
             this.updateEventCatched |= args.Updated.Count > 0;
@@ -91,7 +98,7 @@ namespace Tests
         {
             Favorite favoriteA = this.AddFavoriteToPrimaryPersistence();
             var favoriteB = this.SecondaryFavorites.FirstOrDefault() as Favorite;
-            this.PrimaryPersistence.Dispatcher.FavoritesChanged += 
+            this.PrimaryPersistence.Dispatcher.FavoritesChanged +=
                 new FavoritesChangedEventHandler(this.OnUpdateAlreadyUpdatedFavoritesChanged);
 
             this.UpdateFavorite(favoriteA, this.PrimaryFavorites);
@@ -114,6 +121,16 @@ namespace Tests
         {
             toUpdate.Name += " Changed";
             targetPersistence.Update(toUpdate);
+        }
+
+        [TestMethod]
+        public void TestMasterPasswordUpdate()
+        {
+            const string dummyPassword = "bbb";
+            Database.UpdateMastrerPassord(dummyPassword);
+            Settings.DatabaseMasterPassword = dummyPassword;
+            bool result = Database.TestConnection();
+            Assert.IsTrue(result, "Couldnt update database master password");
         }
     }
 }
