af66ff994bae05709ab39629bfc29fff0b2d30f3
JirkaPok <jirkapok@users.noreply.github.com>
2012-11-21 02:12:27 +0000

    Extended password tests. Implemented all passwordfunctions v2. Passed all passwords tests except config file upgrade.

--- Source/Tests/PasswordTests.cs
+++ Source/Tests/PasswordTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Terminals.Security;
 
@@ -9,70 +10,139 @@ namespace Tests
     /// and upgrade doesn't break any stored passwords
     /// </summary>
     [TestClass]
-    public class PasswordV3Tests
+    public class PasswordTests
     {
-        private const string TEST_PASSWORD = "testPassword";
-        
+        private const string USERPASSWORD = "testPassword";
+        private const string MASTERPASSWORD = "testMaster";
+
         public TestContext TestContext { get; set; }
 
         [TestMethod]
-        public void MasterPasswordKeyV2StillWorksTest()
+        public void V1MasterPasswordValidationTest()
         {
             // cant use persistence security here, because of initialization depends on newest algorithm
-            string key1 = PasswordFunctions.CalculateMasterPasswordKey(TEST_PASSWORD);
-            string key2 = PasswordFunctions.CalculateMasterPasswordKey(TEST_PASSWORD);
-            Assert.AreEqual(key1, key2, "generated master password key v2 doesn't equals.");
+            string masterHash = PasswordFunctions.ComputeMasterPasswordHash(MASTERPASSWORD);
+            string masterHash2 = PasswordFunctions.ComputeMasterPasswordHash(MASTERPASSWORD);
+            Assert.AreEqual(masterHash, masterHash2);
 
-            string encryptedPassword = PasswordFunctions.EncryptPassword(TEST_PASSWORD, key1);
-            string encryptedPassword2 = PasswordFunctions.EncryptPassword(TEST_PASSWORD, key1);
-            Assert.AreEqual(encryptedPassword, encryptedPassword2, "password encryption doesn't generate identical encrypted bytes");
-            
-            string resolvedPassowrd = PasswordFunctions.DecryptPassword(encryptedPassword, key1);
-            Assert.AreEqual(TEST_PASSWORD, resolvedPassowrd, "Unable to restore password");
+            bool isValid = PasswordFunctions.MasterPasswordIsValid(MASTERPASSWORD, masterHash);
+            Assert.IsTrue(isValid, "Couldn't validate stored master password v1");
         }
 
         [TestMethod]
-        public void MasterPasswordKeyV3IsUniqueTest()
+        public void V1MasterPasswordIsUniqueTest()
         {
-            string key1 = PasswordFunctions2.CalculateMasterPasswordKey(TEST_PASSWORD);
-            string key2 = PasswordFunctions2.CalculateMasterPasswordKey(TEST_PASSWORD);
-            Assert.AreNotEqual(key1, key2, "generated master password key always equals.");
+            string key1 = PasswordFunctions.CalculateMasterPasswordKey(MASTERPASSWORD);
+            string key2 = PasswordFunctions.CalculateMasterPasswordKey(MASTERPASSWORD);
+            Assert.AreEqual(key1, key2, "generated master password key v1 doesn't equals.");
         }
 
         [TestMethod]
-        public void MasterPasswordSecretV3ValidationTest()
+        public void V1PasswordsUniqueEncryptionTest()
         {
-            string stored = PasswordFunctions2.ComputeMasterPasswordHash(TEST_PASSWORD);
-            string stored2 = PasswordFunctions2.ComputeMasterPasswordHash(TEST_PASSWORD);
+            string key = PasswordFunctions.CalculateMasterPasswordKey(MASTERPASSWORD);
+            string encryptedPassword = PasswordFunctions.EncryptPassword(USERPASSWORD, key);
+            string encryptedPassword2 = PasswordFunctions.EncryptPassword(USERPASSWORD, key);
+            Assert.AreEqual(encryptedPassword, encryptedPassword2, "password encryption v1 doesn't generate identical encrypted bytes");
+        }
+
+        [TestMethod]
+        public void V1PasswordsEncryptDecryptTest()
+        {
+            PasswordsEncryptDecryptCheck(CheckEncryptDecryptPasswordV1);
+
+            string masterKey = PasswordFunctions.CalculateMasterPasswordKey(MASTERPASSWORD);
+            string decryptedPassword = PasswordFunctions.DecryptPassword(string.Empty, masterKey);
+            Assert.AreEqual(string.Empty, decryptedPassword, "Decryption of empty stored password failed");
+        }
+
+        private static string CheckEncryptDecryptPasswordV1(string password, string masterPassword)
+        {
+            string masterKey = PasswordFunctions.CalculateMasterPasswordKey(masterPassword);
+            string encryptedPassword = PasswordFunctions.EncryptPassword(password, masterKey);
+            string decryptedPassword = PasswordFunctions.DecryptPassword(encryptedPassword, masterKey);
+            Assert.AreEqual(password, decryptedPassword, "Unable to decrypt V1 password");
+            return encryptedPassword;
+        }
+
+        [TestMethod]
+        public void V2MasterPasswordValidationTest()
+        {
+            string stored = PasswordFunctions2.ComputeMasterPasswordStoredKey(MASTERPASSWORD);
+            string stored2 = PasswordFunctions2.ComputeMasterPasswordStoredKey(MASTERPASSWORD);
             Assert.AreNotEqual(stored, stored2);
 
-            string key1 = PasswordFunctions2.CalculateMasterPasswordKey(TEST_PASSWORD);
-            string key2 = PasswordFunctions2.CalculateMasterPasswordKey(TEST_PASSWORD);
-            Assert.AreNotEqual(key1, key2, "generated master password key v2 doesn't equals.");
-
-            string encryptedPassword = PasswordFunctions2.EncryptPassword(TEST_PASSWORD, key1);
-            string encryptedPassword2 = PasswordFunctions2.EncryptPassword(TEST_PASSWORD, key1);
-            Assert.AreNotEqual(encryptedPassword, encryptedPassword2, "password encryption always generates identical encrypted bytes");
-
-            string resolvedPassowrd = PasswordFunctions2.DecryptPassword(encryptedPassword, key1);
-            Assert.AreEqual(TEST_PASSWORD, resolvedPassowrd, "Unable to restore password");
-
-            bool isValid = PasswordFunctions2.MasterPasswordIsValid(TEST_PASSWORD, stored);
-            Assert.IsTrue(isValid, "Unable to validate encrypted master password after version 2");
-            // known shared password was stored properly as stored master password
-            // we are able to decrypt it properly
-            // the same stored password always generates unique encrypted bytes
+            bool isValid = PasswordFunctions2.MasterPasswordIsValid(MASTERPASSWORD, stored);
+            Assert.IsTrue(isValid, "Unable to validate encrypted master password version 2");
         }
 
         [TestMethod]
-        public void UpgradeV2PasswordsTest()
+        public void V2MasterPasswordIsUniqueTest()
         {
+            string key1 = PasswordFunctions2.CalculateMasterPasswordKey(MASTERPASSWORD);
+            string key2 = PasswordFunctions2.CalculateMasterPasswordKey(MASTERPASSWORD);
+            Assert.AreNotEqual(key1, key2, "generated master password key always equals.");
+
+            var accessor = new PrivateType(typeof(PasswordFunctions2));
+            byte[] keySalt = (byte[])accessor.InvokeStatic("CreateRandomKeySalt", new Object[] {});
+            key1 = CalculateV2Key(accessor, keySalt);
+            key2 = CalculateV2Key(accessor, keySalt);
+            Assert.AreEqual(key1, key2, "generated master password doesn't equal.");
+        }
+
+        private static string CalculateV2Key(PrivateType accessor, byte[] keySalt)
+        {
+            object[] parameters = new object[] {MASTERPASSWORD, keySalt};
+            return accessor.InvokeStatic("CalculateMasterPasswordKey", parameters).ToString();
+        }
+
+        [TestMethod]
+        public void V2PasswordsUniqueEncryptionTest()
+        {
+            string key = PasswordFunctions2.CalculateMasterPasswordKey(MASTERPASSWORD);
+            string encryptedPassword = PasswordFunctions2.EncryptPassword(USERPASSWORD, key);
+            string encryptedPassword2 = PasswordFunctions2.EncryptPassword(USERPASSWORD, key);
+            Assert.AreNotEqual(encryptedPassword, encryptedPassword2, "password encryption always generates identical encrypted bytes");
+        }
+
+        [TestMethod]
+        public void V2UpgradePasswordsTest()
+        {
+            string targetDir = Path.Combine(this.TestContext.TestDir, "Terminals.config");
             throw new NotImplementedException();
             // deploy test version 2.0 config file
-            // check, if are able to read all passwords
+            // check, if we are able to read all passwords (validate using old version)
             // use upgrade routine to ensure, that we are able to:
-            // - validate the master password
+            // - update the stored master password
             // - all stored passwords in favorite, credentials, RDP protocol properties are still valid
+        }
+
+        [TestMethod]
+        public void V2PasswordsEncryptDecryptTest()
+        {
+            PasswordsEncryptDecryptCheck(CheckEncryptDecryptPasswordV2);
+
+            string masterKey = PasswordFunctions2.CalculateMasterPasswordKey(MASTERPASSWORD);
+            string decryptedPassword = PasswordFunctions2.DecryptPassword(string.Empty, masterKey);
+            Assert.AreEqual(string.Empty, decryptedPassword, "Decryption of empty stored password failed");
+        }
+
+        private static string CheckEncryptDecryptPasswordV2(string password, string masterPassword)
+        {
+            string masterKey = PasswordFunctions2.CalculateMasterPasswordKey(masterPassword);
+            string encryptedPassword = PasswordFunctions2.EncryptPassword(password, masterKey);
+            string decryptedPassword = PasswordFunctions2.DecryptPassword(encryptedPassword, masterKey);
+            Assert.AreEqual(password, decryptedPassword, "Unable to decrypt password");
+            return encryptedPassword;
+        }
+
+        private static void PasswordsEncryptDecryptCheck(Func<string, string, string> checkEncryptDecryptPassword)
+        {
+            checkEncryptDecryptPassword(USERPASSWORD, MASTERPASSWORD);
+            string encryptedPassword = checkEncryptDecryptPassword(USERPASSWORD, string.Empty);
+            Assert.AreNotEqual(string.Empty, encryptedPassword, "Encrypted password by empty master password isn't valid");
+            encryptedPassword = checkEncryptDecryptPassword(string.Empty, MASTERPASSWORD);
+            Assert.AreEqual(string.Empty, encryptedPassword, "Encrypted empty user password isn't valid");
         }
     }
 }
