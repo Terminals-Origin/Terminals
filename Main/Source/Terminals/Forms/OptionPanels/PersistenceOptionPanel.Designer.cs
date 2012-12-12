6a25465c9fe9d59ff3d0907313b48fe70ae87768
jirkapok_cp <SND\jirkapok_cp>
2012-12-12 16:16:50 +0000

    Added new Advanced connection string properties form.

--- Main/Source/Terminals/Forms/OptionPanels/PersistenceOptionPanel.Designer.cs
+++ Main/Source/Terminals/Forms/OptionPanels/PersistenceOptionPanel.Designer.cs
@@ -33,27 +33,28 @@ namespace Terminals.Forms
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBoxPersistenceType = new System.Windows.Forms.GroupBox();
             this.SqlServerOptionsPanel = new System.Windows.Forms.Panel();
+            this.databaseCombobox = new System.Windows.Forms.ComboBox();
             this.QueryLabel = new System.Windows.Forms.Label();
+            this.TableQueryLabel = new System.Windows.Forms.Label();
             this.serversComboBox = new System.Windows.Forms.ComboBox();
-            this.SearchButton = new System.Windows.Forms.Button();
+            this.btnDatabases = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.TestLabel = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.btnCreateNew = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SqlServerAuthPanel = new System.Windows.Forms.Panel();
-            this.databaseCombobox = new System.Windows.Forms.ComboBox();
-            this.TableQueryLabel = new System.Windows.Forms.Label();
-            this.buttonDatabases = new System.Windows.Forms.Button();
-            this.TestLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.SqlServerPasswordTextBox = new System.Windows.Forms.TextBox();
             this.SqlServerUserNameTextBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.bntTestSqlConnection = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.bntTestSqlConnection = new System.Windows.Forms.Button();
             this.SqlServerAuthenticationComboBox = new System.Windows.Forms.ComboBox();
             this.lblRestart = new System.Windows.Forms.Label();
             this.rbtnSqlPersistence = new System.Windows.Forms.RadioButton();
             this.rbtnFilePersistence = new System.Windows.Forms.RadioButton();
+            this.btnAdvanced = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.groupBoxPersistenceType.SuspendLayout();
             this.SqlServerOptionsPanel.SuspendLayout();
@@ -85,15 +86,16 @@ namespace Terminals.Forms
             // 
             // SqlServerOptionsPanel
             // 
+            this.SqlServerOptionsPanel.Controls.Add(this.btnAdvanced);
             this.SqlServerOptionsPanel.Controls.Add(this.databaseCombobox);
             this.SqlServerOptionsPanel.Controls.Add(this.QueryLabel);
             this.SqlServerOptionsPanel.Controls.Add(this.TableQueryLabel);
             this.SqlServerOptionsPanel.Controls.Add(this.serversComboBox);
-            this.SqlServerOptionsPanel.Controls.Add(this.buttonDatabases);
-            this.SqlServerOptionsPanel.Controls.Add(this.SearchButton);
+            this.SqlServerOptionsPanel.Controls.Add(this.btnDatabases);
+            this.SqlServerOptionsPanel.Controls.Add(this.btnSearch);
             this.SqlServerOptionsPanel.Controls.Add(this.TestLabel);
             this.SqlServerOptionsPanel.Controls.Add(this.label5);
-            this.SqlServerOptionsPanel.Controls.Add(this.button1);
+            this.SqlServerOptionsPanel.Controls.Add(this.btnCreateNew);
             this.SqlServerOptionsPanel.Controls.Add(this.label1);
             this.SqlServerOptionsPanel.Controls.Add(this.SqlServerAuthPanel);
             this.SqlServerOptionsPanel.Controls.Add(this.label2);
@@ -105,6 +107,14 @@ namespace Terminals.Forms
             this.SqlServerOptionsPanel.Size = new System.Drawing.Size(427, 209);
             this.SqlServerOptionsPanel.TabIndex = 17;
             // 
+            // databaseCombobox
+            // 
+            this.databaseCombobox.FormattingEnabled = true;
+            this.databaseCombobox.Location = new System.Drawing.Point(117, 125);
+            this.databaseCombobox.Name = "databaseCombobox";
+            this.databaseCombobox.Size = new System.Drawing.Size(173, 21);
+            this.databaseCombobox.TabIndex = 23;
+            // 
             // QueryLabel
             // 
             this.QueryLabel.AutoSize = true;
@@ -115,6 +125,16 @@ namespace Terminals.Forms
             this.QueryLabel.Text = "Querying...";
             this.QueryLabel.Visible = false;
             // 
+            // TableQueryLabel
+            // 
+            this.TableQueryLabel.AutoSize = true;
+            this.TableQueryLabel.Location = new System.Drawing.Point(332, 128);
+            this.TableQueryLabel.Name = "TableQueryLabel";
+            this.TableQueryLabel.Size = new System.Drawing.Size(58, 13);
+            this.TableQueryLabel.TabIndex = 21;
+            this.TableQueryLabel.Text = "Querying...";
+            this.TableQueryLabel.Visible = false;
+            // 
             // serversComboBox
             // 
             this.serversComboBox.FormattingEnabled = true;
@@ -123,15 +143,35 @@ namespace Terminals.Forms
             this.serversComboBox.Size = new System.Drawing.Size(173, 21);
             this.serversComboBox.TabIndex = 2;
             // 
-            // SearchButton
+            // btnDatabases
             // 
-            this.SearchButton.Location = new System.Drawing.Point(296, 12);
-            this.SearchButton.Name = "SearchButton";
-            this.SearchButton.Size = new System.Drawing.Size(29, 23);
-            this.SearchButton.TabIndex = 3;
-            this.SearchButton.Text = "...";
-            this.SearchButton.UseVisualStyleBackColor = true;
-            this.SearchButton.Click += new System.EventHandler(this.SearchButtonClick);
+            this.btnDatabases.Location = new System.Drawing.Point(297, 123);
+            this.btnDatabases.Name = "btnDatabases";
+            this.btnDatabases.Size = new System.Drawing.Size(29, 23);
+            this.btnDatabases.TabIndex = 22;
+            this.btnDatabases.Text = "...";
+            this.btnDatabases.UseVisualStyleBackColor = true;
+            this.btnDatabases.Click += new System.EventHandler(this.ButtonDatabasesClick);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(296, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(29, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "...";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.SearchButtonClick);
+            // 
+            // TestLabel
+            // 
+            this.TestLabel.AutoSize = true;
+            this.TestLabel.Location = new System.Drawing.Point(362, 157);
+            this.TestLabel.Name = "TestLabel";
+            this.TestLabel.Size = new System.Drawing.Size(51, 13);
+            this.TestLabel.TabIndex = 21;
+            this.TestLabel.Text = "Testing...";
+            this.TestLabel.Visible = false;
             // 
             // label5
             // 
@@ -141,6 +181,15 @@ namespace Terminals.Forms
             this.label5.Size = new System.Drawing.Size(56, 13);
             this.label5.TabIndex = 16;
             this.label5.Text = "Database:";
+            // 
+            // btnCreateNew
+            // 
+            this.btnCreateNew.Location = new System.Drawing.Point(175, 152);
+            this.btnCreateNew.Name = "btnCreateNew";
+            this.btnCreateNew.Size = new System.Drawing.Size(115, 23);
+            this.btnCreateNew.TabIndex = 9;
+            this.btnCreateNew.Text = "Create New...";
+            this.btnCreateNew.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -162,53 +211,6 @@ namespace Terminals.Forms
             this.SqlServerAuthPanel.Name = "SqlServerAuthPanel";
             this.SqlServerAuthPanel.Size = new System.Drawing.Size(410, 55);
             this.SqlServerAuthPanel.TabIndex = 16;
-            // 
-            // DatabaseCombobox
-            // 
-            this.databaseCombobox.FormattingEnabled = true;
-            this.databaseCombobox.Location = new System.Drawing.Point(117, 125);
-            this.databaseCombobox.Name = "databaseCombobox";
-            this.databaseCombobox.Size = new System.Drawing.Size(173, 21);
-            this.databaseCombobox.TabIndex = 23;
-            // 
-            // TableQueryLabel
-            // 
-            this.TableQueryLabel.AutoSize = true;
-            this.TableQueryLabel.Location = new System.Drawing.Point(332, 128);
-            this.TableQueryLabel.Name = "TableQueryLabel";
-            this.TableQueryLabel.Size = new System.Drawing.Size(58, 13);
-            this.TableQueryLabel.TabIndex = 21;
-            this.TableQueryLabel.Text = "Querying...";
-            this.TableQueryLabel.Visible = false;
-            // 
-            // buttonDatabases
-            // 
-            this.buttonDatabases.Location = new System.Drawing.Point(297, 123);
-            this.buttonDatabases.Name = "buttonDatabases";
-            this.buttonDatabases.Size = new System.Drawing.Size(29, 23);
-            this.buttonDatabases.TabIndex = 22;
-            this.buttonDatabases.Text = "...";
-            this.buttonDatabases.UseVisualStyleBackColor = true;
-            this.buttonDatabases.Click += new System.EventHandler(this.ButtonDatabasesClick);
-            // 
-            // TestLabel
-            // 
-            this.TestLabel.AutoSize = true;
-            this.TestLabel.Location = new System.Drawing.Point(362, 157);
-            this.TestLabel.Name = "TestLabel";
-            this.TestLabel.Size = new System.Drawing.Size(51, 13);
-            this.TestLabel.TabIndex = 21;
-            this.TestLabel.Text = "Testing...";
-            this.TestLabel.Visible = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(175, 152);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Create New...";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -243,6 +245,15 @@ namespace Terminals.Forms
             this.label4.TabIndex = 14;
             this.label4.Text = "Password:";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Authentication:";
+            // 
             // bntTestSqlConnection
             // 
             this.bntTestSqlConnection.Enabled = false;
@@ -253,15 +264,6 @@ namespace Terminals.Forms
             this.bntTestSqlConnection.Text = "Test...";
             this.bntTestSqlConnection.UseVisualStyleBackColor = true;
             this.bntTestSqlConnection.Click += new System.EventHandler(this.OnBntTestSqlConnectionClick);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 42);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Authentication:";
             // 
             // SqlServerAuthenticationComboBox
             // 
@@ -310,6 +312,16 @@ namespace Terminals.Forms
             this.rbtnFilePersistence.UseVisualStyleBackColor = true;
             this.rbtnFilePersistence.CheckedChanged += new System.EventHandler(this.OnRbtnFilePersistenceCheckedChanged);
             // 
+            // btnAdvanced
+            // 
+            this.btnAdvanced.Location = new System.Drawing.Point(94, 152);
+            this.btnAdvanced.Name = "btnAdvanced";
+            this.btnAdvanced.Size = new System.Drawing.Size(75, 23);
+            this.btnAdvanced.TabIndex = 24;
+            this.btnAdvanced.Text = "Advanced";
+            this.btnAdvanced.UseVisualStyleBackColor = true;
+            this.btnAdvanced.Click += new System.EventHandler(this.ButtonAdvancedClick);
+            // 
             // PersistenceOptionPanel
             // 
             this.Controls.Add(this.panel1);
@@ -343,14 +355,15 @@ namespace Terminals.Forms
         private Label label2;
         private Label label1;
         private Panel SqlServerOptionsPanel;
-        private Button button1;
+        private Button btnCreateNew;
         private Label label5;
-        private Button SearchButton;
+        private Button btnSearch;
         private ComboBox serversComboBox;
         private Label QueryLabel;
         private Label TestLabel;
         private Label TableQueryLabel;
-        private Button buttonDatabases;
+        private Button btnDatabases;
         private ComboBox databaseCombobox;
+        private Button btnAdvanced;
     }
 }
---
+++ Main/Source/Terminals/Forms/SqlConnectionForm.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Terminals.Forms
+{
+    /// <summary>
+    /// Handles detailed possibility to configure all properties of MS SQL connection using property grid.
+    /// Reused idea from Visual Studio Server Explorer.
+    /// </summary>
+    internal partial class SqlConnectionForm : Form
+    {
+        internal SqlConnectionStringBuilder DataSource
+        {
+            get
+            {
+                return this.pGridConnection.SelectedObject as SqlConnectionStringBuilder;
+            }
+            set
+            {
+                if (value == null)
+                    this.pGridConnection.SelectedObject = new SqlConnectionStringBuilder();
+                else
+                    this.pGridConnection.SelectedObject = value;
+
+                this.UpdateConnectionStringTextBox();
+            }
+        }
+
+        internal SqlConnectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PropertyGridConnectionPropertyValueChanged(object s, PropertyValueChangedEventArgs e)
+        {
+            this.UpdateConnectionStringTextBox();
+        }
+
+        private void UpdateConnectionStringTextBox()
+        {
+            if (this.DataSource != null)
+            {
+                this.txtCurrenConnectionString.Text = this.DataSource.ToString();
+            }
+        }
+    }
+}
