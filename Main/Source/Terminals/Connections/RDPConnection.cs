613526ac62dcb75034f4ae5648c4bbe526e0ca0b
jirkapok_cp <SND\jirkapok_cp>
2013-03-03 20:59:55 +0000

    Extracted error messages resolution from rdpConnection.

--- Main/Source/Terminals/Connections/RDPConnection.cs
+++ Main/Source/Terminals/Connections/RDPConnection.cs
@@ -416,27 +416,12 @@ namespace Terminals.Connections
         private void ShowDisconnetMessageBox(IMsTscAxEvents_OnDisconnectedEvent e, AxMsRdpClient6 client)
         {
             int reason = e.discReason;
-            string error = ToDisconnectMessage(client, reason);
+            string error = RdpClientErrorMessages.ToDisconnectMessage(client, reason);
             
             if (!string.IsNullOrEmpty(error))
             {
                 string message = String.Format("Error connecting to {0}\n\n{1}", client.Server, error);
                 MessageBox.Show(this, message, Program.Info.TitleVersion, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
-        }
-
-        private static string ToDisconnectMessage(AxMsRdpClient6 client, int reason)
-        {
-            switch (reason)
-            {
-                case 1:
-                case 2:
-                case 3:
-                    // These are normal disconnects and not considered errors.
-                    return string.Empty;
-
-                default:
-                    return client.GetErrorDescription((uint)reason, (uint)client.ExtendedDisconnectReason);
             }
         }
 
@@ -461,101 +446,26 @@ namespace Terminals.Connections
         private void client_OnFatalError(object sender, IMsTscAxEvents_OnFatalErrorEvent e)
         {
             int errorCode = e.errorCode;
-            string message = ToFatalErrorMessage(errorCode);
+            string message = RdpClientErrorMessages.ToFatalErrorMessage(errorCode);
             string finalMsg = string.Format("There was a fatal error returned from the RDP Connection, details:\n\nError Code:{0}\n\nError Description:{1}", errorCode, message);
             MessageBox.Show(finalMsg);
             Logging.Log.Fatal(finalMsg);
         }
 
-        private static string ToFatalErrorMessage(int errorCode)
-        {
-            switch (errorCode)
-            {
-                case 0:
-                    return "An unknown error has occurred.";
-                case 1:
-                    return "Internal error code 1.";
-                case 2:
-                    return "An out-of-memory error has occurred.";
-                case 3:
-                    return "A window-creation error has occurred.";
-                case 4:
-                    return "Internal error code 2.";
-                case 5:
-                    return "Internal error code 3. This is not a valid state.";
-                case 6:
-                    return "Internal error code 4.";
-                case 7:
-                    return "An unrecoverable error has occurred during client connection.";
-                case 100:
-                    return "Winsock initialization error.";
-                default:
-                    return "An unknown error.";
-            }
-        }
-
         private void client_OnWarning(object sender, IMsTscAxEvents_OnWarningEvent e)
         {
             int warningCode = e.warningCode;
-            string message = ToWarningMessage(warningCode);
+            string message = RdpClientErrorMessages.ToWarningMessage(warningCode);
             string finalMsg = string.Format("There was a warning returned from the RDP Connection, details:\n\nWarning Code:{0}\n\nWarning Description:{1}", warningCode, message);
             Logging.Log.Warn(finalMsg);
-        }
-
-        private static string ToWarningMessage(int warningCode)
-        {
-            switch (warningCode)
-            {
-                case 1:
-                    return "Bitmap cache is corrupt.";
-                default:
-                    return "An unknown warning";
-            }
         }
 
         private void client_OnLogonError(object sender, IMsTscAxEvents_OnLogonErrorEvent e)
         {
             int errorCode = e.lError;
-            string message = ToLogonMessage(errorCode);
+            string message = RdpClientErrorMessages.ToLogonMessage(errorCode);
             string finalMsg = string.Format("There was a logon error returned from the RDP Connection, details:\n\nLogon Code:{0}\n\nLogon Description:{1}", errorCode, message);
             Logging.Log.Error(finalMsg);
-        }
-
-        private static string ToLogonMessage(int errorCode)
-        {
-            switch (errorCode)
-            {
-                case -5:
-                    return "Winlogon is displaying the Session Contention dialog box.";
-                case -2:
-                    return "Winlogon is continuing with the logon process.";
-                case -3:
-                    return "Winlogon is ending silently.";
-                case -6:
-                    return "Winlogon is displaying the No Permissions dialog box.";
-                case -7:
-                    return "Winlogon is displaying the Disconnect Refused dialog box.";
-                case -4:
-                    return "Winlogon is displaying the Reconnect dialog box.";
-                case -1:
-                    return "The user was denied access.";
-                case 0:
-                    return "The logon failed because the logon credentials are not valid.";
-                case 2:
-                    return "Another logon or post-logon error occurred. The Remote Desktop client displays a logon screen to the user.";
-                case 1:
-                    return "The password is expired. The user must update their password to continue logging on.";
-                case 3:
-                    return "The Remote Desktop client displays a dialog box that contains important information for the user.";
-                case -1073741714:
-                    return "The user name and authentication information are valid, but authentication was blocked due to restrictions on the user account, such as time-of-day restrictions.";
-                case -1073741715:
-                    return "The attempted logon is not valid. This is due to either an incorrect user name or incorrect authentication information.";
-                case -1073741276:
-                    return "The password is expired. The user must update their password to continue logging on.";
-                default:
-                    return "An unknown error.";
-            }
         }
 
         #endregion
