83455b3e88fd8f5660466ba030700ca54c317c7a
JirkaPok <jirkapok@users.noreply.github.com>
2013-04-04 10:50:05 +0000

    Created new unit test to check, that RdpOptions have persistence security assigned after procol type change in favorite.

---
+++ Source/Tests/FilePersisted/FilePersistedTestLab.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terminals.Configuration;
+using Terminals.Data;
+
+namespace Tests.FilePersisted
+{
+    public abstract class FilePersistedTestLab
+    {
+        internal FilePersistence Persistence { get; private set; }
+
+        [TestInitialize]
+        public void InitializeTestLab()
+        {
+            SetDefaultFileLocations();
+            this.Persistence = new FilePersistence();
+        }
+
+        internal static void SetDefaultFileLocations()
+        {
+            Settings.FileLocations.AssignCustomFileLocations(string.Empty, string.Empty, string.Empty);
+        }
+
+        internal IFavorite AddFavorite()
+        {
+            IFavorite favorite = this.Persistence.Factory.CreateFavorite();
+            this.Persistence.Favorites.Add(favorite);
+            return favorite;
+        }
+    }
+}
