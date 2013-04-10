54d29f6a8a21c247afec9b0c1814b5ed245326bd
jirkapok_cp <SND\jirkapok_cp>
2013-04-10 15:21:59 +0000

    Fixed Length of the Protocol property in database model to reflect latest DB value (max. 10 chars) to accept all protocols. Added new Version property to the Group Db model.

--- Main/Source/Terminals/Data/DB/DbGroup.cs
+++ Main/Source/Terminals/Data/DB/DbGroup.cs
@@ -22,6 +22,7 @@ namespace Terminals.Data.DB
     
         public int Id { get; set; }
         public string Name { get; set; }
+        public byte[] Version { get; set; }
     
         public virtual ICollection<DbGroup> ChildGroups { get; set; }
         public virtual DbGroup ParentGroup { get; set; }
