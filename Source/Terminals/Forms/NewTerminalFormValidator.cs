cad35ab3e081db8a1198062d9f4a485ecc410ef4
JirkaPok <jirkapok@users.noreply.github.com>
2013-06-14 01:41:02 +0000

    Added errorProvider to the NewTerminal form. Finished validation refactoring in newTerminal form.

--- Source/Terminals/Forms/NewTerminalFormValidator.cs
+++ Source/Terminals/Forms/NewTerminalFormValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Terminals.Connections;
 using Terminals.Data.Validation;
@@ -17,59 +18,59 @@ namespace Terminals.Forms
             this.form = form;
         }
 
-        internal bool HasErrors()
+        internal void OnServerNameValidating(object sender, CancelEventArgs eventArgs)
         {
-            return !this.IsServerNameValid(this.form.ProtocolText, this.form.ServerNameText) ||
-                   !this.IsPortValid(this.form.PortText);
+            const string MESSAGE = "Server name is required and has to be valid computer name or IP adress.";
+            this.IsValid(this.IsServerNameValid, sender, MESSAGE);
         }
 
-        internal bool IsServerNameValid(string protocolToSet, string serverName)
+        internal void OnUrlValidating(object sender, CancelEventArgs eventArgs)
         {
-            if (ConnectionManager.IsProtocolWebBased(protocolToSet))
+            this.IsValid(this.IsUrlValid, sender, "Server URL isnt in valid format.");
+        }
+
+        internal void OnPortValidating(object sender, CancelEventArgs eventArgs)
+        {
+           this.IsValid(this.IsPortValid, sender, "Port must be a number between 0 and 65535");
+        }
+
+        private void IsValid(Func<bool> isValid, object sender, string message)
+        {
+            string errorMessage = isValid() ? string.Empty : message;
+            this.form.SetErrorInfo(sender, errorMessage);
+        }
+
+        internal bool IsServerNameValid()
+        {
+            string protocol = this.form.ProtocolText;
+            if(ConnectionManager.IsProtocolWebBased(protocol))
                 return true;
-
-            return this.IsServerNameValid(serverName.Trim());
+            
+            return this.ServerNameInvalid();
         }
 
-        private bool IsServerNameValid(string serverName)
+        private bool ServerNameInvalid()
         {
-            if (IsServerNameEmpty(serverName))
-            {
-                this.form.RerportErrorInServerName("Server name was not specified.");
-                return false;
-            }
-
-            if (CustomValidationRules.IsValidServerName(serverName) != ValidationResult.Success)
-            {
-                this.form.RerportErrorInServerName("Server name is not in the correct format.");
-                return false;
-            }
-
-            return true;
+            string serverName = this.form.ServerNameText;
+            return !IsServerNameEmpty() || CustomValidationRules.IsValidServerName(serverName) == ValidationResult.Success;
         }
 
-        internal bool IsServerNameEmpty(string serverName)
+        internal bool IsServerNameEmpty()
         {
-            return String.IsNullOrEmpty(serverName);
+            return String.IsNullOrEmpty(this.form.ServerNameText);
         }
 
-        private bool IsPortValid(string portText)
+        private bool IsPortValid()
         {
             Int32 result;
-            bool isPortValid = Int32.TryParse(portText, out result) && result >= 0 && result <= 65535;
-
-            if (isPortValid)
-                return true;
-
-            this.form.ShowErrorMessageBox("Port must be a number between 0 and 65535");
-            return false;
+            bool parsed = Int32.TryParse(this.form.PortText, out result);
+            return parsed && result >= 0 && result <= 65535;
         }
 
         internal bool IsUrlValid()
         {
             Uri url = this.form.GetFullUrlFromHttpTextBox();
-            bool urlValid = url != null;
-            return urlValid;
+            return url != null;
         }
     }
 }
