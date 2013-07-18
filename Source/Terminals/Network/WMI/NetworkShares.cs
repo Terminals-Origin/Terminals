9d106b9006d112830ff4a5288982d88d90059107
JirkaPok <jirkapok@users.noreply.github.com>
2013-07-18 00:13:53 +0000

    Nework shares resolution code split to be able handle exceptions

--- Source/Terminals/Network/WMI/NetworkShares.cs
+++ Source/Terminals/Network/WMI/NetworkShares.cs
@@ -11,74 +11,93 @@ namespace Terminals.Network
         {
             InitializeComponent();
         }
+
         private void LoadShares(string username, string password, string computer)
         {
             List<Share> shares = new List<Share>();
-            const string QRY = "select * from win32_share";
-            ManagementObjectSearcher searcher;
-            ObjectQuery query = new ObjectQuery(QRY);
-
-            if (username != "" && password != "" && computer != "" && !computer.StartsWith(@"\\localhost"))
-            {
-                ConnectionOptions oConn = new ConnectionOptions();
-                oConn.Username = username;
-                oConn.Password = password;
-                if (!computer.StartsWith(@"\\")) 
-                    computer = @"\\" + computer;
-                if (!computer.ToLower().EndsWith(@"\root\cimv2"))
-                    computer = computer + @"\root\cimv2";
-                ManagementScope oMs = new ManagementScope(computer, oConn);
-
-                searcher = new ManagementObjectSearcher(oMs, query);
-            }
-            else
-            {
-                searcher = new ManagementObjectSearcher(query);
-            }
+            ManagementObjectSearcher searcher = CreateSearcher(username, password, computer);
 
             foreach (ManagementObject share in searcher.Get())
             {
-                Share s = new Share();
-                foreach (PropertyData p in share.Properties)
-                {
-                    switch (p.Name)
-                    {
-                        case "AccessMask":
-                            if (p.Value != null) s.AccessMask = p.Value.ToString();
-                            break;
-                        case "MaximumAllowed":
-                            if (p.Value != null) s.MaximumAllowed = p.Value.ToString();
-                            break;
-                        case "InstallDate":
-                            if (p.Value != null) s.InstallDate = p.Value.ToString();
-                            break;
-                        case "Description":
-                            if (p.Value != null) s.Description = p.Value.ToString();
-                            break;
-                        case "Caption":
-                            if (p.Value != null) s.Caption = p.Value.ToString();
-                            break;
-                        case "AllowMaximum":
-                            if (p.Value != null) s.AllowMaximum = p.Value.ToString();
-                            break;
-                        case "Name":
-                            if (p.Value != null) s.Name = p.Value.ToString();
-                            break;
-                        case "Path":
-                            if (p.Value != null) s.Path = p.Value.ToString();
-                            break;
-                        case "Status":
-                            if (p.Value != null) s.Status = p.Value.ToString();
-                            break;
-                        case "Type":
-                            if (p.Value != null) s.TypeId = p.Value.ToString();
-                            break;
-                    }
-                }
+                var s = new Share();
+                FillShareByManagementObject(share, s);
                 shares.Add(s);
             }
+
             this.dataGridView1.DataSource = shares;
         }
+
+        private static void FillShareByManagementObject(ManagementObject share, Share s)
+        {
+            foreach (PropertyData property in share.Properties)
+            {
+                switch (property.Name)
+                {
+                    case "AccessMask":
+                        s.AccessMask = ResolvePropertyValue(property);
+                        break;
+                    case "MaximumAllowed":
+                        s.MaximumAllowed = ResolvePropertyValue(property);
+                        break;
+                    case "InstallDate":
+                        s.InstallDate = ResolvePropertyValue(property);
+                        break;
+                    case "Description":
+                        s.Description = ResolvePropertyValue(property);
+                        break;
+                    case "Caption":
+                        s.Caption = ResolvePropertyValue(property);
+                        break;
+                    case "AllowMaximum":
+                        s.AllowMaximum = ResolvePropertyValue(property);
+                        break;
+                    case "Name":
+                        s.Name = ResolvePropertyValue(property);
+                        break;
+                    case "Path":
+                        s.Path = ResolvePropertyValue(property);
+                        break;
+                    case "Status":
+                        s.Status = ResolvePropertyValue(property);
+                        break;
+                    case "Type":
+                        s.TypeId = ResolvePropertyValue(property);
+                        break;
+                }
+            }
+        }
+
+        private static string ResolvePropertyValue(PropertyData property)
+        {
+            if (property.Value != null)
+                return property.Value.ToString();
+            return string.Empty;
+        }
+
+        private static ManagementObjectSearcher CreateSearcher(string username, string password, string computer)
+        {
+            var query = new ObjectQuery("select * from win32_share");
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) ||
+                string.IsNullOrEmpty(computer) || computer.StartsWith(@"\\localhost"))
+                return new ManagementObjectSearcher(query);
+
+            return CreateRemoteSearcher(username, password, computer, query);
+        }
+
+        private static ManagementObjectSearcher CreateRemoteSearcher(string username, string password,
+            string computer, ObjectQuery query)
+        {
+            var oConn = new ConnectionOptions();
+            oConn.Username = username;
+            oConn.Password = password;
+            if (!computer.StartsWith(@"\\"))
+                computer = @"\\" + computer;
+            if (!computer.ToLower().EndsWith(@"\root\cimv2"))
+                computer = computer + @"\root\cimv2";
+            var oMs = new ManagementScope(computer, oConn);
+            return new ManagementObjectSearcher(oMs, query);
+        }
+
         private void NetworkShares_Load(object sender, EventArgs e)
         {
             this.LoadShares(string.Empty, string.Empty, string.Empty);
