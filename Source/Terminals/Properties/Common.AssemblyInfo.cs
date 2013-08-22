ca6bb812e015b5c6ff04ca6e615b07859df3eb78
JirkaPok <jirkapok@users.noreply.github.com>
2013-08-22 12:55:57 +0000

    Version number upgrade to 3.3

--- Source/Terminals/Properties/Common.AssemblyInfo.cs
+++ Source/Terminals/Properties/Common.AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 [assembly: AssemblyCompany("Robert Chartier")]
 [assembly: AssemblyProduct("Terminals by Robert Chartier")]
-[assembly: AssemblyCopyright("Copyright © by Robert Chartier 2006 - 2012")]
-[assembly: AssemblyVersion("3.2.0.*")]
+[assembly: AssemblyCopyright("Copyright © by Robert Chartier 2006 - 2013")]
+[assembly: AssemblyVersion("3.3.0.*")]
