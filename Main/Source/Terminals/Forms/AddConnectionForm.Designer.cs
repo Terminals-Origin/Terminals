2835bfa8174d3da65281a91c0cba16a0ccffdf22
jirkapok_cp <SND\jirkapok_cp>
2013-09-01 21:39:10 +0000

    Replaced grid in Add favorite to group by the search panel.

--- Main/Source/Terminals/Forms/AddConnectionForm.Designer.cs
+++ Main/Source/Terminals/Forms/AddConnectionForm.Designer.cs
@@ -29,54 +29,12 @@ namespace Terminals
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddConnectionForm));
-            this.gbFavorites = new System.Windows.Forms.GroupBox();
-            this.gridFavorites = new System.Windows.Forms.DataGridView();
-            this.colFavoriteName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnOk = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.gbFavorites.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gridFavorites)).BeginInit();
+            this.searchPanel = new Terminals.Forms.Controls.SearchPanel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // gbFavorites
-            // 
-            this.gbFavorites.Controls.Add(this.gridFavorites);
-            this.gbFavorites.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gbFavorites.Location = new System.Drawing.Point(0, 0);
-            this.gbFavorites.Name = "gbFavorites";
-            this.gbFavorites.Size = new System.Drawing.Size(349, 315);
-            this.gbFavorites.TabIndex = 0;
-            this.gbFavorites.TabStop = false;
-            this.gbFavorites.Text = "Favorites";
-            // 
-            // gridFavorites
-            // 
-            this.gridFavorites.AllowUserToAddRows = false;
-            this.gridFavorites.AllowUserToDeleteRows = false;
-            this.gridFavorites.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.gridFavorites.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridFavorites.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.colFavoriteName});
-            this.gridFavorites.Location = new System.Drawing.Point(7, 21);
-            this.gridFavorites.Name = "gridFavorites";
-            this.gridFavorites.ReadOnly = true;
-            this.gridFavorites.RowHeadersVisible = false;
-            this.gridFavorites.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.gridFavorites.Size = new System.Drawing.Size(336, 288);
-            this.gridFavorites.TabIndex = 0;
-            this.gridFavorites.SelectionChanged += new System.EventHandler(this.GridFavorites_SelectionChanged);
-            // 
-            // colFavoriteName
-            // 
-            this.colFavoriteName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.colFavoriteName.DataPropertyName = "Name";
-            this.colFavoriteName.HeaderText = "Favorite name";
-            this.colFavoriteName.Name = "colFavoriteName";
-            this.colFavoriteName.ReadOnly = true;
             // 
             // panel1
             // 
@@ -112,6 +70,15 @@ namespace Terminals
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
+            // searchPanel
+            // 
+            this.searchPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.searchPanel.Location = new System.Drawing.Point(0, 0);
+            this.searchPanel.Name = "searchPanel";
+            this.searchPanel.Size = new System.Drawing.Size(349, 315);
+            this.searchPanel.TabIndex = 2;
+            this.searchPanel.ResultsSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.SearchPanel_ResultsSelectionChanged);
+            // 
             // AddConnectionForm
             // 
             this.AcceptButton = this.btnOk;
@@ -119,7 +86,7 @@ namespace Terminals
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(349, 362);
-            this.Controls.Add(this.gbFavorites);
+            this.Controls.Add(this.searchPanel);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -128,9 +95,9 @@ namespace Terminals
             this.Name = "AddConnectionForm";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Select favorite to add";
-            this.gbFavorites.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gridFavorites)).EndInit();
+            this.Text = "Select favorites to add";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AddConnectionForm_FormClosing);
+            this.Load += new System.EventHandler(this.AddConnectionForm_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -138,11 +105,9 @@ namespace Terminals
 
         #endregion
 
-        private System.Windows.Forms.GroupBox gbFavorites;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.DataGridView gridFavorites;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colFavoriteName;
+        private Forms.Controls.SearchPanel searchPanel;
     }
 }
