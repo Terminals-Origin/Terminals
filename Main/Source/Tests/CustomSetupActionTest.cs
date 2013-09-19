c4c69208f02df71918708e91ec4d2c80b833de49
jirkapok_cp <SND\jirkapok_cp>
2013-09-19 23:59:38 +0000

    Split of setup action tests.

--- Main/Source/Tests/CustomSetupActionTest.cs
+++ Main/Source/Tests/CustomSetupActionTest.cs
@@ -14,41 +14,88 @@ namespace Tests
         private string log4NetFile;
         private string appConfigFile;
         private string targetDir;
+
+        // backup original content into memory
+        private string appConfigContent;
+        private string log4NetContent;
+
         public TestContext TestContext { get; set; }
+
+        private string DeployDirectory
+        {
+            get { return this.TestContext.DeploymentDirectory; }
+        }
 
         [TestInitialize]
         public void TestInitialize()
         {
-            this.targetDir = this.TestContext.DeploymentDirectory;
-            this.log4NetFile = Path.Combine(targetDir, "Terminals.log4net.config");
-            this.appConfigFile = Path.Combine(targetDir, "Terminals.exe.config");
-            File.Move(Path.Combine(targetDir, "app.config"), appConfigFile);
-            File.SetAttributes(log4NetFile, FileAttributes.Normal);
-            File.SetAttributes(appConfigFile, FileAttributes.Normal);
+            this.PrepareTargetDir();
+            // we always have to deploy both files, even if we test only one,
+            // because the tested method requires both of them
+            this.PrepareLo4NetFile();
+            this.PrepareAppConfigFile();
+        }
+
+        private void PrepareAppConfigFile()
+        {
+            this.appConfigFile = Path.Combine(this.targetDir, "Terminals.exe.config");
+            File.Copy(Path.Combine(DeployDirectory, "app.config"), this.appConfigFile);
+            File.SetAttributes(this.appConfigFile, FileAttributes.Normal);
+            this.appConfigContent = File.ReadAllText(this.appConfigFile);
+        }
+
+        private void PrepareLo4NetFile()
+        {
+            const string LO4_NET_FILENAME = "Terminals.log4net.config";
+            this.log4NetFile = Path.Combine(this.targetDir, LO4_NET_FILENAME);
+            File.Copy(Path.Combine(DeployDirectory, LO4_NET_FILENAME), this.log4NetFile);
+            File.SetAttributes(this.log4NetFile, FileAttributes.Normal);
+            this.log4NetContent = File.ReadAllText(this.log4NetFile);
+        }
+
+        private void PrepareTargetDir()
+        {
+            // use random directory name to allow concurrent run of both tests
+            string testDir = Path.GetRandomFileName();
+            this.targetDir = Path.Combine(this.DeployDirectory, testDir);
+            Directory.CreateDirectory(this.targetDir);
         }
 
         [TestMethod]
-        public void UserProfileVersionTest()
+        public void PortableVersionAppconfigTest()
         {
-            // backup both into memory
-            string appConfigContent = File.ReadAllText(appConfigFile);
-            string log4NetContent = File.ReadAllText(log4NetFile);
-
             // first option without changes
-            UpgradeConfigFiles.CheckPortableInstallType(targetDir, false);
-            string upgradedLog4Net = File.ReadAllText(log4NetFile);
-            Assert.AreEqual(log4NetContent, upgradedLog4Net, "Log4net config file is not expected default");
+            UpgradeConfigFiles.CheckPortableInstallType(this.targetDir, false);
             string upgradedAppConfig = File.ReadAllText(appConfigFile);
-            Assert.AreEqual(appConfigContent, upgradedAppConfig, "App config file is not expected default");
+            Assert.AreEqual(appConfigContent, upgradedAppConfig, "App config file is expected default");
+        }
 
+        [TestMethod]
+        public void PortableVersionLog4NetTest()
+        {
+            UpgradeConfigFiles.CheckPortableInstallType(this.targetDir, false);
+            string upgradedLog4Net = File.ReadAllText(log4NetFile);
+            Assert.AreEqual(log4NetContent, upgradedLog4Net, "Log4net config file is expected default");
+        }
+
+        [TestMethod]
+        public void UserProfileVersionAppConfigTest()
+        {
             // second option, which should contain the requried user profile options
             UpgradeConfigFiles.CheckPortableInstallType(targetDir, true);
-            upgradedAppConfig = File.ReadAllText(appConfigFile);
-            bool inUserProfile = upgradedAppConfig.Contains("<value>False</value>"); // the only option in the file
+            string upgradedAppConfig = File.ReadAllText(appConfigFile);
+            // the only option in the file
+            bool inUserProfile = upgradedAppConfig.Contains("<value>False</value>");
             Assert.IsTrue(inUserProfile, "App config doesnt contain user profile flag");
+        }
 
-            upgradedLog4Net = File.ReadAllText(log4NetFile);
-            bool oldPath = upgradedLog4Net.Contains(@"\Robert_Chartier\Terminals\Data\logs\"); // part of the upgraded path
+        [TestMethod]
+        public void UserProfileVersionLog4NetTest()
+        {
+            UpgradeConfigFiles.CheckPortableInstallType(targetDir, true);
+            string upgradedLog4Net = File.ReadAllText(log4NetFile);
+            // part of the upgraded path
+            bool oldPath = upgradedLog4Net.Contains(@"\Robert_Chartier\Terminals\Data\logs\");
             Assert.IsTrue(oldPath, "Log4net config doesnt contain user profile path");
         }
     }
