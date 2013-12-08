045834a60d8887d6e48c625fca93750a5213e773
JirkaPok <jirkapok@users.noreply.github.com>
2013-12-08 01:21:28 +0000

    Removed Escape shortcut key to close organize favorites form and Addconnectionform because of duplicity with cancel search.

--- Source/Terminals/Forms/AddConnectionForm.Designer.cs
+++ Source/Terminals/Forms/AddConnectionForm.Designer.cs
@@ -84,7 +84,6 @@ namespace Terminals
             this.AcceptButton = this.btnOk;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(349, 362);
             this.Controls.Add(this.searchPanel);
             this.Controls.Add(this.panel1);
