288167c84718d6fefd818e857fe176b8b272bb46
JirkaPok <jirkapok@users.noreply.github.com>
2014-04-24 23:15:00 +0000

    Created favorite content  import skeleton in rdcMan

--- Source/Terminals/Integration/Import/RdcManFavoriteImporter.cs
+++ Source/Terminals/Integration/Import/RdcManFavoriteImporter.cs
@@ -14,11 +14,92 @@ namespace Terminals.Integration.Import
 
         internal FavoriteConfigurationElement ImportServer()
         {
+            ImportGeneralProperties();
+            //ImportConnectionSettings();
+            //ImportLocalResources();
+            //ImportCredentials();
+            //ImportGatewaySettings();
+            //ImportDesktopSettings();
+            return this.favorite;
+        }
+
+        private void ImportGeneralProperties()
+        {
             this.favorite.Name = this.server.DisplayName;
             this.favorite.ServerName = this.server.Name;
-            // todo import favorite content
+            this.favorite.Notes = this.server.Comment;
+        }
 
-            return this.favorite;
+        private void ImportConnectionSettings()
+        {
+            ConnectionSettings connectionSettings = this.server.ConnectionSettings;
+            this.favorite.ConnectToConsole = connectionSettings.ConnectToConsole;
+            this.favorite.Port =  connectionSettings.Port;
+            this.favorite.SecurityWorkingFolder = connectionSettings.StartDir;
+            this.favorite.SecurityStartProgram = connectionSettings.StartProgram;
+        }
+
+        private void ImportLocalResources()
+        {
+            LocalResources localResources = this.server.LocalResources;
+            this.favorite.RedirectSmartCards = localResources.RedirectSmartCards;
+            this.favorite.RedirectPrinters = localResources.RedirectPrinters;
+            this.favorite.RedirectPorts = localResources.RedirectPorts;
+            this.favorite.RedirectClipboard = localResources.RedirectClipboard;
+            // todo is the mapping correct of keyboardHook?
+            this.favorite.GrabFocusOnConnect = localResources.KeyboardHook == 0;
+        }
+
+        private void ImportCredentials()
+        {
+            LogonCredentials credentials = this.server.LogonCredentials;
+            this.favorite.DomainName = credentials.Domain;
+            this.favorite.UserName = credentials.UserName;
+            this.favorite.Password = credentials.Password;
+        }
+
+        private void ImportGatewaySettings()
+        {
+            GatewaySettings tsgwSettings = this.server.GatewaySettings;
+            this.favorite.TsgwSeparateLogin = !string.IsNullOrEmpty(tsgwSettings.UserName);
+            this.favorite.TsgwCredsSource = tsgwSettings.LogonMethod;
+            this.favorite.TsgwHostname = tsgwSettings.HostName;
+            this.favorite.TsgwDomain = tsgwSettings.Domain;
+            this.favorite.TsgwUsername = tsgwSettings.UserName;
+        }
+
+        private void ImportDesktopSettings()
+        {
+            RdcManRemoteDesktop desktopSettings = this.server.RemoteDesktop;
+            this.favorite.DesktopSize = this.ConvertSizeToDeskopSize(desktopSettings);
+            this.favorite.Colors = this.ConvertColorDepthToColors(desktopSettings.ColorDept);
+        }
+
+        private Colors ConvertColorDepthToColors(int colorDept)
+        {
+            return Colors.Bits32;
+        }
+
+        private DesktopSize ConvertSizeToDeskopSize(RdcManRemoteDesktop desktopSettings)
+        {
+            if (desktopSettings.FullScreen)
+            {
+                return DesktopSize.FullScreen;
+            }
+             
+            // "800 x 600"
+            // 1024 x 768
+            // 1280 x 800
+            // 1280 x 1024
+            // 1440 x 900
+            // 1600 x 1200
+            // 1680 x 1050
+            // 1920 x 1200
+            // same as client arrea
+            // full screen 
+            // custom
+
+            return DesktopSize.AutoScale;
         }
     }
 }
