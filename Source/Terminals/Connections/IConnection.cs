d242d7a81a56e79c5c6fd9fd217a097c203ea6ff
JirkaPok <jirkapok@users.noreply.github.com>
2014-10-14 23:47:59 +0000

    Code cleanup in Connection and its interface.

--- Source/Terminals/Connections/IConnection.cs
+++ Source/Terminals/Connections/IConnection.cs
@@ -5,16 +5,11 @@ namespace Terminals.Connections
 {
     internal interface IConnection
     {
-        TerminalTabControlItem TerminalTabPage { get; set; }
-        IFavorite Favorite { get; set; }
-        MainForm ParentForm { get; set; }
-        bool Connect();
-        void Disconnect();
+        IFavorite Favorite { get; }
+        TerminalServer Server { get; }
+        bool IsTerminalServer { get; }
         bool Connected { get; }
-        void ChangeDesktopSize(DesktopSize Size);
-        TerminalServer Server { get; set; }
-        bool IsTerminalServer { get; set; }
-
-        string LastError { get; set; }
+        void Disconnect();
+        void ChangeDesktopSize(DesktopSize size);
     }
 }
