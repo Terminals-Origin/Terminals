f3bc94984745196ec47ba7a2fbfed872b1ca3c6f
KarlosChuar_cp <SND\KarlosChuar_cp>
2014-10-22 19:36:43 +0000

    Fixed RdpConnection Dispose, fixed Dispose call in MainForm.

--- Main/Source/Terminals/Connections/IConnection.cs
+++ Main/Source/Terminals/Connections/IConnection.cs
@@ -4,12 +4,26 @@ using Terminals.TerminalServices;
 
 namespace Terminals.Connections
 {
+    /// <summary>
+    /// The connection is closed by its explicit call to Dispose,
+    /// because most connections work with unmanaged resources and we usualy dont reconnect.
+    /// Instead we close the current connection and create new instance.
+    /// So there is no oposit to Connect method.
+    /// </summary>
     internal interface IConnection : IDisposable
     {
         IFavorite Favorite { get; }
         TerminalServer Server { get; }
         bool IsTerminalServer { get; }
         bool Connected { get; }
+
+        /// <summary>
+        /// Create connection doesnt mean to open the connection.
+        /// Use explicit call instead. Because there may be related resources, 
+        /// call <see cref="IDisposable.Dispose"/> to close the connection to prevent memory leak.
+        /// </summary>
+        bool Connect();
+
         void ChangeDesktopSize(DesktopSize size);
     }
 }
