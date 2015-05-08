b7abf863259d46ececebc198105723546df47169
KarlosChuar_cp <SND\KarlosChuar_cp>
2015-05-08 20:41:47 +0000

    Created CaptureManger integration test.

---
+++ Main/Source/Tests/UserInterface/CaptureManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terminals.CaptureManager;
+using Terminals.Configuration;
+using Tests.FilePersisted;
+
+namespace Tests.UserInterface
+{
+    [TestClass]
+    public class CaptureManagerTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void NoTab_PerformScreenCapture_TakesScreenShot()
+        {
+            this.ConfigureSettings();
+            var expectedFiles = this.GetFilesCountInCaptureDirectory() + 1;
+            var tabControl = new TabControl.TabControl();
+            CaptureManager.PerformScreenCapture(tabControl);
+            var actualFiles = this.GetFilesCountInCaptureDirectory();
+            Assert.AreEqual(expectedFiles, actualFiles, "Screen capture should create new file in capture direcotry.");
+        }
+
+        private void ConfigureSettings()
+        {
+            FilePersistedTestLab.SetDefaultFileLocations();
+            var settings = Settings.Instance;
+            settings.CaptureRoot = this.TestContext.TestDeploymentDir;
+            settings.EnableCaptureToFolder = true;
+        }
+
+        private int GetFilesCountInCaptureDirectory()
+        {
+            return Directory.GetFiles(this.TestContext.TestDeploymentDir).Length;
+        }
+    }
+}
