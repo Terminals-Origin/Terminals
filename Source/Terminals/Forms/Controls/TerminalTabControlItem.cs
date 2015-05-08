8fe0643a6b82ece3db691a7e56e3cea484b5d9bf
JirkaPok <jirkapok@users.noreply.github.com>
2015-05-08 20:41:47 +0000

    Created CaptureManger integration test.

--- Source/Terminals/Forms/Controls/TerminalTabControlItem.cs
+++ Source/Terminals/Forms/Controls/TerminalTabControlItem.cs
@@ -6,8 +6,16 @@ namespace Terminals
 {
     internal class TerminalTabControlItem : TabControlItem
     {
+        /// <summary>
+        /// Gets or sets the associated favorite.
+        /// Not all tabs are using it, so it is optional.
+        /// </summary>
         public IConnection Connection { get; set; }
 
+        /// <summary>
+        /// Gets or sets the associated favorite.
+        /// Not all tabs are using it, so it is optional.
+        /// </summary>
         public IFavorite Favorite { get; set; }
 
         public TerminalTabControlItem(string caption)
