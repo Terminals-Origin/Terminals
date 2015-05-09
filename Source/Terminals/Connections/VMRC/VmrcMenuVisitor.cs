24b4c58ed98f856eb05859c7a0fc1893eb54c12a
JirkaPok <jirkapok@users.noreply.github.com>
2015-05-09 21:32:00 +0000

    Added skeleton for toolbar customization by plugins

---
+++ Source/Terminals/Connections/VMRC/VmrcMenuVisitor.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Terminals.Connections
+{
+    internal class VmrcMenuVisitor
+    {
+        internal void UpdateMenu(ToolStrip standardToolbar)
+        {
+            
+        }
+    }
+}
---
+++ Source/Tests/UserInterface/VmrcMenuVisitorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terminals.Connections;
+
+namespace Tests.UserInterface
+{
+    [TestClass]
+    public class VmrcMenuVisitorTests
+    {
+        [TestMethod]
+        public void EmptyToolBar_Visit_CreatesNewButtons()
+        {
+            ToolStrip standardToolbar = new ToolStrip();
+            var menuVisitor = new VmrcMenuVisitor();
+            menuVisitor.UpdateMenu(standardToolbar);
+            int itemsCount = standardToolbar.Items.Count;
+            Assert.AreEqual(2, itemsCount, "First visit should update the menu by addint its own menu items");
+        }
+    }
+}
