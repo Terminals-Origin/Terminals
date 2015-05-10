ec6735c0f595588cc21d0fde57d0e7cf2c95bc75
JirkaPok <jirkapok@users.noreply.github.com>
2015-05-10 19:30:40 +0000

    Removed redundant test, because the Visible property is always false if the toolstrip is not on visible form.

--- Source/Tests/UserInterface/VmrcMenuVisitorTests.cs
+++ Source/Tests/UserInterface/VmrcMenuVisitorTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Terminals.Connections;
@@ -9,8 +8,6 @@ namespace Tests.UserInterface
     [TestClass]
     public class VmrcMenuVisitorTests
     {
-        private const string ACTIVE_BUTTONS_MESSAGE = "Its commands should be visible only in case the active connection is VMRC";
-
         private Mock<IConnection> mockConnection;
 
         private Mock<ICurrenctConnectionProvider> mockProvider;
@@ -25,14 +22,6 @@ namespace Tests.UserInterface
             this.menuVisitor.UpdateMenu(this.toolbar);
             int itemsCount = this.toolbar.Items.Count;
             Assert.AreEqual(2, itemsCount, "First visit should update the menu by addint its own menu items");
-        }
-
-        [TestMethod]
-        public void NoConnection_UpdateMenu_HidesButtons()
-        {
-            this.menuVisitor.UpdateMenu(this.toolbar);
-            bool allHiden = this.toolbar.Items.OfType<ToolStripButton>().All(mi => !mi.Visible);
-            Assert.IsTrue(allHiden, ACTIVE_BUTTONS_MESSAGE);
         }
 
         [TestInitialize]
