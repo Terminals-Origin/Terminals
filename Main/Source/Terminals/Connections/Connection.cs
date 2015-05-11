6a2e8913bd2c9e17d94006cc348e5b942c81bcdf
KarlosChuar_cp <SND\KarlosChuar_cp>
2015-05-11 22:52:14 +0000

    Moved Terminal server service from base class to the RdpConnection

--- Main/Source/Terminals/Connections/Connection.cs
+++ Main/Source/Terminals/Connections/Connection.cs
@@ -31,42 +31,12 @@ namespace Terminals.Connections
 
         public IConnectionMainView ParentForm { get; set; }
 
-        public TerminalServer Server { get; private set; }
-
-        public bool IsTerminalServer { get; private set; }
-
         /// <summary>
         /// Create this control doesnt mean to open the connection.
         /// Use explicit call instead. Because there may be related resources, 
         /// call Dispose to close the connection to prevent memory leak.
         /// </summary>
         public abstract bool Connect();
-
-        public void CheckForTerminalServer(IFavorite favorite)
-        {
-            if (favorite.Protocol == ConnectionManager.RDP)
-                ThreadPool.QueueUserWorkItem(new WaitCallback(this.CheckForTS), favorite);
-
-            this.IsTerminalServer = false;
-        }
-    
-        private void CheckForTS(object state)
-        {
-            this.IsTerminalServer = false;
-            IFavorite favorite = state as IFavorite;
-            try
-            {
-                Thread.Sleep(3000);
-                this.Server = TerminalServer.LoadServer(favorite.ServerName);
-                this.IsTerminalServer = this.Server.IsATerminalServer;
-            }
-            catch (Exception exception)
-            {
-                string message = string.Format("Checked to see if {0} is a terminal server. {0} is not a terminal server",
-                                               favorite.ServerName);
-                Logging.Error(message, exception);
-            }
-        }
 
         protected void Log(string text)
         {
