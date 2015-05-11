939b2c13c0f0b4db11acb33c9789734d4d2d517b
JirkaPok <jirkapok@users.noreply.github.com>
2015-05-11 21:19:48 +0000

    Removed duplicit code from menu visitors tests.

--- Source/Tests/UserInterface/VmrcMenuVisitorTests.cs
+++ Source/Tests/UserInterface/VmrcMenuVisitorTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Windows.Forms;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Terminals.Connections;
 
 namespace Tests.UserInterface
@@ -8,31 +6,14 @@ namespace Tests.UserInterface
     [TestClass]
     public class VmrcMenuVisitorTests
     {
-        private Mock<IConnection> mockConnection;
-
-        private Mock<ICurrenctConnectionProvider> mockProvider;
-
-        private ToolStrip toolbar;
-
-        private VmrcMenuVisitor menuVisitor;
-
         [TestMethod]
         public void EmptyToolBar_UpdateMenu_CreatesNewButtons()
         {
-            this.menuVisitor.Visit(this.toolbar);
-            int itemsCount = this.toolbar.Items.Count;
+            var sut = new MenuVisitorSut();
+            var menuVisitor = new VmrcMenuVisitor(sut.MockProvider.Object);
+            menuVisitor.Visit(sut.Toolbar);
+            int itemsCount = sut.Toolbar.Items.Count;
             Assert.AreEqual(2, itemsCount, "First visit should update the menu by addint its own menu items");
-        }
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            this.mockConnection = new Mock<IConnection>();
-            this.mockProvider = new Mock<ICurrenctConnectionProvider>();
-            this.mockProvider.SetupGet(p => p.CurrentConnection)
-                .Returns(() => this.mockConnection.Object); // other connection, than null
-            this.toolbar = new ToolStrip();
-            this.menuVisitor = new VmrcMenuVisitor(this.mockProvider.Object);
         }
     }
 }
