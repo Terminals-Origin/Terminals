719ce5c1eb6a0d62fdb672bd810aa89099df40a6
KarlosChuar_cp <SND\KarlosChuar_cp>
2015-10-23 22:06:24 +0000

    Extracted https plugin

---
+++ Main/Source/Terminals/Connections/Web/HttpsConnectionPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace Terminals.Connections.Web
+{
+    internal class HttpsConnectionPlugin: IConnectionPlugin
+    {
+        public int Port { get { return ConnectionManager.HTTPSPort; } }
+
+        public string PortName { get { return ConnectionManager.HTTPS; } }
+
+        public Connection CreateConnection()
+        {
+            return new HTTPConnection();
+        }
+
+        public Control[] CreateOptionsControls()
+        {
+            return new Control[0];
+        }
+    }
+}
