3e41f2c347eba76233277321ce1d6d7d5bf89205
KarlosChuar_cp <SND\KarlosChuar_cp>
2015-10-23 23:04:07 +0000

    extracted rdp connection plugin

---
+++ Main/Source/Terminals/Connections/Rdp/RdpConnectionPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using Terminals.Forms.EditFavorite;
+
+namespace Terminals.Connections.Rdp
+{
+    internal class RdpConnectionPlugin : IConnectionPlugin
+    {
+        public int Port { get { return ConnectionManager.RDPPort; } }
+
+        public string PortName { get { return ConnectionManager.RDP; } }
+
+        public Connection CreateConnection()
+        {
+            // return new FakeRdpConnection();
+            return new RDPConnection();
+        }
+
+        public Control[] CreateOptionsControls()
+        {
+            return new Control[]
+            {
+                new RdpDisplayControl() { Name = "Display" },
+                new RdpExtendedSettingsControl() { Name = "Extended settings" },
+                new RdpLocalResourcesControl() { Name = "Local resources" },
+                new RdpSecurityControl() { Name = "Security" },
+                new RdpTsGatewayControl() { Name = "TS Gateway" }
+            };
+        }
+    }
+}
