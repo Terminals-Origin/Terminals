6141749e17a4e9f9c07154f0fdf15160607a1795
KarlosChuar_cp <SND\KarlosChuar_cp>
2015-10-23 22:53:05 +0000

    extracted ica connection plugin

---
+++ Main/Source/Terminals/Connections/ICA/ICAConnectionPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using Terminals.Forms.EditFavorite;
+
+namespace Terminals.Connections.ICA
+{
+    internal class ICAConnectionPlugin :IConnectionPlugin
+    {
+        public int Port { get { return ConnectionManager.ICAPort; } }
+
+        public string PortName { get { return ConnectionManager.ICA_CITRIX; } }
+
+        public Connection CreateConnection()
+        {
+            return new ICAConnection();
+        }
+
+        public Control[] CreateOptionsControls()
+        {
+            return new Control[] { new CitrixControl() { Name = "ICA Citrix" } };
+        }
+    }
+}
