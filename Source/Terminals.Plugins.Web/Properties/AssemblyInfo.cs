c690271c4306a0c482b92df3fa4416af4a92e2a4
JirkaPok <jirkapok@users.noreply.github.com>
2016-02-11 23:38:18 +0000

    Added new Web Plugin project.

---
+++ Source/Terminals.Plugins.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Terminals.Plugins.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("278face8-1579-4c13-ae59-1b24a7091cfc")]
