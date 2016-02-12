2f0649ece6a247501f11faf537b0b1dca2236fa6
JirkaPok <jirkapok@users.noreply.github.com>
2016-02-12 00:11:00 +0000

    Created new project for Ica plugin.

---
+++ Source/Terminals.Plugins.Ica/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Terminals.Plugins.Ica")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb4a4cc7-c449-4f81-8395-7806cc1bdfa8")]
