17ef1c6d06c9e8024f52bbb9a3d93110463e20aa
JirkaPok <jirkapok@users.noreply.github.com>
2016-02-12 21:56:29 +0000

    Created new project for Terminal plugin

---
+++ Source/Terminals.Plugins.Terminal/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Terminals.Plugins.Terminal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e972a0c-57e2-4e5c-9ef5-d7cd681188c5")]
+
+[assembly: InternalsVisibleTo("Terminals")]
+[assembly: InternalsVisibleTo("Tests")]
