ec212a04baa3a1d5a8433a7dea13873e5aee605c
KarlosChuar_cp <SND\KarlosChuar_cp>
2016-12-16 00:19:20 +0000

    Added chocolatey package creation scripts

---
+++ Main/Build/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'terminals' # arbitrary name for the package, used in messages
+$installerType = 'msi' #only one of these: exe, msi, msu
+$url = 'http://terminals.codeplex.com/downloads/get/' # download url
+$silentArgs = '/qn /norestart' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
