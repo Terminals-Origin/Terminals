5d859f89b3768ab59efb68da92d1de27ee848997
KarlosChuar_cp <SND\KarlosChuar_cp>
2016-12-22 00:46:25 +0000

    Split of Validation tests to respective files.

---
+++ Main/Source/Tests/Validation/DbValidationTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terminals.Data.DB;
+using Terminals.Data.Validation;
+using Tests.Connections;
+
+namespace Tests.Validation
+{
+    [TestClass]
+    internal class DbValidationTests : ValidationTests
+    {
+        private readonly DbValidations validator = new DbValidations(TestConnectionManager.Instance);
+
+        [TestMethod]
+        public void LongDbGroupName_ValidateNameOnly_ReturnsNameError()
+        {
+            var group = new DbGroup();
+            group.Name = LongText;
+            AssertNameOnlyValidation(this.validator, group);
+        }
+
+        [TestMethod]
+        public void DbCredential_Validate_ReturnsError()
+        {
+            AssertCredentialsValidation(this.validator, new DbCredentialSet(), 2);
+        }
+
+        [TestMethod]
+        public void EmptyDbGroupName_ValidateGroup_ReturnsError()
+        {
+            AssertGroupValidation(this.validator, new DbGroup());
+        }
+
+        [TestMethod]
+        public void InvalidDbFavorite_Validatie_ReturnsErrorsForAllProperties()
+        {
+            ValidationStates results = ValidateDbFavorite();
+            Assert.AreEqual(9, results.Count(), "Some properties arent validated properly for DbFavorite");
+        }
+
+        [TestMethod]
+        public void DbFavorite_Validate_ReturnsAllServerNameErrors()
+        {
+            ValidationStates results = ValidateDbFavorite();
+            var serverNameErrors = results.Count(result => result.PropertyName == "ServerName");
+            Assert.AreEqual(2, serverNameErrors, "DbFavorite ServerName wasnt validated properly");
+        }
+
+        [TestMethod]
+        public void DbFavorite_Validate_ReturnsAllProtocolErrors()
+        {
+            ValidationStates results = ValidateDbFavorite();
+            var protocolErrors = results.Count(result => result.PropertyName == "Protocol");
+            Assert.AreEqual(2, protocolErrors, "DbFavorite protocol wasnt validated properly");
+        }
+
+        private ValidationStates ValidateDbFavorite()
+        {
+            // created dbfavorite is not compleate, only necessary to make validable using IFavorite
+            var favorite = new DbFavorite();
+            favorite.ExecuteBeforeConnect = new DbBeforeConnectExecute();
+            favorite.Security = new DbSecurityOptions();
+            favorite.Details.LoadFieldsFromReferences();
+
+            favorite.Protocol = LongText.Substring(0, 11);
+            favorite.ServerName = LongText;
+            favorite.Name = LongText;
+            favorite.Notes = LongText;
+
+            favorite.ExecuteBeforeConnect.Command = LongText;
+            favorite.ExecuteBeforeConnect.CommandArguments = LongText;
+            favorite.ExecuteBeforeConnect.InitialDirectory = LongText;
+
+            return validator.Validate(favorite);
+        }
+
+    }
+}
--- Main/Source/Tests/Validation/ValidationTests.cs
+++ Main/Source/Tests/Validation/ValidationTests.cs
@@ -3,20 +3,23 @@ using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Terminals.Data;
-using Terminals.Data.DB;
 using Terminals.Data.Interfaces;
 using Terminals.Data.Validation;
-using Tests.Connections;
 
 namespace Tests.Validation
 {
     [TestClass]
-    public class ValidationTests
+    internal class ValidationTests
     {
-        private static string longText;
+        protected static string LongText { get; set; }
 
         [ClassInitialize]
         public static void InitializeLongText(TestContext context)
+        {
+            LongText = GenerateLongText();
+        }
+
+        private static string GenerateLongText()
         {
             StringBuilder longTextBuilder = new StringBuilder();
             for (int index = 0; index < 500; index++)
@@ -24,117 +27,24 @@ namespace Tests.Validation
                 longTextBuilder.Append(index.ToString());
             }
 
-            longText = longTextBuilder.ToString();
+            return longTextBuilder.ToString();
         }
 
-        [TestMethod]
-        public void Favorite_Validate_ReturnsErrors()
-        {
-            var favorite = new Favorite();
-            favorite.Protocol = longText.Substring(0, 11);
-            favorite.ServerName = longText;
-            var results = new FileValidations(TestConnectionManager.Instance).Validate(favorite);
-            Assert.AreEqual(3, results.Count(), "Some properties arent validated properly for Favorite");
-        }
-
-        [TestMethod]
-        public void InvalidDbFavorite_Validatie_ReturnsErrorsForAllProperties()
-        {
-            ValidationStates results = ValidateDbFavorite();
-            Assert.AreEqual(9, results.Count(), "Some properties arent validated properly for DbFavorite");
-        }
-
-        [TestMethod]
-        public void DbFavorite_Validate_ReturnsAllServerNameErrors()
-        {
-            ValidationStates results = ValidateDbFavorite();
-            var serverNameErrors = results.Count(result => result.PropertyName == "ServerName");
-            Assert.AreEqual(2, serverNameErrors, "DbFavorite ServerName wasnt validated properly");
-        }
-
-        [TestMethod]
-        public void DbFavorite_Validate_ReturnsAllProtocolErrors()
-        {
-            ValidationStates results = ValidateDbFavorite();
-            var protocolErrors = results.Count(result => result.PropertyName == "Protocol");
-            Assert.AreEqual(2, protocolErrors, "DbFavorite protocol wasnt validated properly");
-        }
-
-        private static ValidationStates ValidateDbFavorite()
-        {
-            // created dbfavorite is not compleate, only necessary to make validable using IFavorite
-            var favorite = new DbFavorite();
-            favorite.ExecuteBeforeConnect = new DbBeforeConnectExecute();
-            favorite.Security = new DbSecurityOptions();
-            favorite.Details.LoadFieldsFromReferences();
-
-            favorite.Protocol = longText.Substring(0, 11);
-            favorite.ServerName = longText;
-            favorite.Name = longText;
-            favorite.Notes = longText;
-
-            favorite.ExecuteBeforeConnect.Command = longText;
-            favorite.ExecuteBeforeConnect.CommandArguments = longText;
-            favorite.ExecuteBeforeConnect.InitialDirectory = longText;
-            var results = new DbValidations(TestConnectionManager.Instance).Validate(favorite);
-            return results;
-        }
-
-        [TestMethod]
-        public void EmptyGroupName_ValidateGroup_ReturnsError()
-        {
-            AssertGroupValidation(new FileValidations(null), new Group());
-        }
-
-        [TestMethod]
-        public void EmptyDbGroupName_ValidateGroup_ReturnsError()
-        {
-            AssertGroupValidation(new DbValidations(null), new DbGroup());
-        }
-
-        private static void AssertGroupValidation(IDataValidator validator, IGroup group)
+        protected static void AssertGroupValidation(IDataValidator validator, IGroup group)
         {
             group.Name = String.Empty;
             var results = validator.Validate(group);
             Assert.AreEqual(1, results.Count, "Group name validation failed");
         }
 
-        [TestMethod]
-        public void Credential_Validate_ReturnsError()
+        protected static void AssertCredentialsValidation(IDataValidator validator, ICredentialSet credentailSet, int expectedErrorsCount)
         {
-            AssertCredentialsValidation(new FileValidations(null), new CredentialSet(), 1);
-        }
-
-        [TestMethod]
-        public void DbCredential_Validate_ReturnsError()
-        {
-            AssertCredentialsValidation(new DbValidations(null), new DbCredentialSet(), 2);
-        }
-
-        private static void AssertCredentialsValidation(IDataValidator validator, ICredentialSet credentailSet, int expectedErrorsCount)
-        {
-            credentailSet.Name = longText;
+            credentailSet.Name = LongText;
             var results = validator.Validate(credentailSet);
             Assert.AreEqual(expectedErrorsCount, results.Count(), "CredentailSet validation failed");
         }
 
-        [TestMethod]
-        public void LongDbGroupName_ValidateNameOnly_ReturnsNameError()
-        {
-            var group = new DbGroup();
-            group.Name = longText;
-            AssertNameOnlyValidation(new DbValidations(null), group);
-        }
-
-        [TestMethod]
-        public void EmptyGroupName_ValidateNameOnly_ReturnsNameError()
-        {
-            var group = new Group();
-            group.Name = string.Empty;
-            AssertNameOnlyValidation(new FileValidations(null), group);
-        }
-
-        private static void AssertNameOnlyValidation(IDataValidator validator, IGroup group)
+        protected static void AssertNameOnlyValidation(IDataValidator validator, IGroup group)
         {
             ValidationStates results = validator.ValidateNameProperty(group);
             Assert.AreEqual(1, results.Count(), "Group name validation failed");
