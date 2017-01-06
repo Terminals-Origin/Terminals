bb5dd60f0df5840f0872e27c5ccdf94bbc3c18d1
KarlosChuar_cp <SND\KarlosChuar_cp>
2017-01-06 18:44:20 +0000

    Added upgrade of chocolatey package to latest version in installprerequisities script

--- Main/Build/InstallPrerequisities.ps1
+++ Main/Build/InstallPrerequisities.ps1
@@ -1,9 +1,11 @@
 # Install prerequisities
 # Terminals environment depends on some external components to be able run the build script
+# Ensure the execution policy is set to unrestricted
 
 # Install chocolatey using Nuget, which is already part of the solution
 ..\Source\.nuget\NuGet.exe install chocolatey -Version 0.9.9.8 -OutputDirectory ..\Source\packages
 ..\Source\packages\chocolatey.0.9.9.8\tools\chocolateyInstall.ps1
+choco upgrade chocolatey -y;
 
 # powershell community extensions to get the write-zip command let
 choco install pscx -y;
