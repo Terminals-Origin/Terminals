7721d5f1b4a695e79e865ab7ccf939f74e5a7fdf
KarlosChuar_cp <SND\KarlosChuar_cp>
2017-01-06 21:35:59 +0000

    Adding test skeleton for Argument builder tests.

---
+++ FeatureBranches/Putty/Source/Tests/Putty/ArgumentBuilderTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Terminals.Data;
+using Terminals.Plugins.Putty;
+
+namespace Tests.Putty
+{
+    [TestClass]
+    public class ArgumentBuilderTests
+    {
+        [TestMethod]
+        public void ValidFavoriteAndCredentials_Build_ReturnsAllSshArguments()
+        {
+            var credentials = new Mock<IGuardedSecurity>();
+            var favorite = new Mock<IFavorite>();
+            var builder = new ArgumentsBuilder(credentials.Object, favorite.Object);
+            string arguments = builder.Build();
+            Assert.AreEqual("commandline", arguments, "All arguments need to be used from favorite and its credentials");
+        }
+    }
+}
