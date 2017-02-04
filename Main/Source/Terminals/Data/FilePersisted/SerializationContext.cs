3c0e1a33a2dee42b0c92dc9aed6fcf50b5187b97
KarlosChuar_cp <SND\KarlosChuar_cp>
2017-02-04 11:01:43 +0000

    Added unit tests for FavoritesSerializer

---
+++ Main/Source/Terminals/Data/FilePersisted/SerializationContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Terminals.Data.FilePersisted
+{
+    internal class SerializationContext
+    {
+        internal FavoritesFile File { get; private set; }
+
+        public List<XElement> Unknown { get; private set; }
+
+        public SerializationContext(FavoritesFile file)
+        {
+            this.File = file;
+            this.Unknown = new List<XElement>();
+        }
+    }
+}
