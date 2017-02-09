13ec6b7d8dc3ef8efdc5219b3d0d80a71ce7890b
KarlosChuar_cp <SND\KarlosChuar_cp>
2017-02-09 22:24:27 +0000

    Implemented loading of unknown favorite group membership

--- Main/Source/Terminals/Data/FilePersisted/FavoritesXmlFile.cs
+++ Main/Source/Terminals/Data/FilePersisted/FavoritesXmlFile.cs
@@ -40,11 +40,35 @@ namespace Terminals.Data.FilePersisted
         internal UnknonwPluginElements RemoveUnknownFavorites(string[] availableProtocols)
         {
             IEnumerable<XElement> favorites = this.SelectElements("//t:Favorite");
-            List<XElement> toFilter = favorites.Where(f => this.IsUnknownProtocol(f, availableProtocols)).ToList();
-            toFilter.ForEach(f => f.Remove());
-            var unknow = new UnknonwPluginElements();
-            unknow.Favorites.AddRange(toFilter);
-            return unknow;
+            List<XElement> unknownFavorites = favorites.Where(f => this.IsUnknownProtocol(f, availableProtocols)).ToList();
+            unknownFavorites.ForEach(f => f.Remove());
+            IEnumerable<XElement> groupMembership = this.SelectElements("//t:FavoritesInGroup");
+            Dictionary<string, List<XElement>> unknownMemberships = this.FilterGroupMembeship(groupMembership, unknownFavorites);
+            return new UnknonwPluginElements(unknownFavorites, unknownMemberships);
+        }
+
+        private Dictionary<string, List<XElement>> FilterGroupMembeship(IEnumerable<XElement> favoritesInGroups, List<XElement> unknownFavorites)
+        {
+            string[] unknownFavoriteIds = unknownFavorites.Select(f => f.Attribute("id").Value).ToArray();
+            return favoritesInGroups.ToDictionary(FindGroupId, fg => FilterUnknownFavoritesForGroup(fg, unknownFavoriteIds));
+        }
+
+        private static string FindGroupId(XElement favoritesInGroup)
+        {
+            return favoritesInGroup.Attribute("groupId").Value;
+        }
+
+        private List<XElement> FilterUnknownFavoritesForGroup(XElement favoritesInGroup, string[] unknownFavoriteIds)
+        {
+            return unknownFavoriteIds.Select(f => this.ExtractFavoriteGuid(favoritesInGroup, f)).ToList();
+        }
+
+        private XElement ExtractFavoriteGuid(XElement favoritesInGroup, string favoriteId)
+        {
+            var unknownFavorite = favoritesInGroup.XPathSelectElements("//t:guid", this.namespaceManager)
+                .First(e => e.Value == favoriteId);
+            unknownFavorite.Remove();
+            return unknownFavorite;
         }
 
         private bool IsUnknownProtocol(XElement favoriteElement, string[] availableProtocols)
