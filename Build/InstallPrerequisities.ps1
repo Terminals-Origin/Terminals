6ccafd601bfe5d89b2c099cef9e5836bdb7284eb
JirkaPok <j.pok@post.cz>
2017-04-12 13:19:09 +0200

    Fixed install prerequisities script by latest command to install chocolatey

--- Build/InstallPrerequisities.ps1
+++ Build/InstallPrerequisities.ps1
@@ -3,7 +3,7 @@
 # Ensure the execution policy is set to unrestricted
 
 # Install chocolatey using Nuget, which is already part of the solution
-..\Source\.nuget\NuGet.exe install chocolatey -Version 0.10.5 -OutputDirectory ..\Source\packages
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 ..\Source\packages\chocolatey.0.10.5\tools\chocolateyInstall.ps1
 choco upgrade chocolatey -y;
 
