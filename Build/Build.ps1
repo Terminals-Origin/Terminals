ff15caa38f778ef0895d28c513224a27a5368655
JirkaPok <j.pok@post.cz>
2017-06-22 01:07:43 +0200

    Updated tests to be still compatible with resharper runner.

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -10,7 +10,7 @@ if(Test-Path .\Output) {
 # Visual Studio 2015
 $msbuild = "c:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe";
 # Compile the solution - the distributionrelease configuration contains installer, which is not normal configurations
-invoke-expression "$msbuild ..\Source\Terminals.sln /m /p:configuration=DistributionRelease /p:Platform='Any CPU' /t:rebuild" | Tee-Object $logFile -Append;
+invoke-expression "$msbuild ..\Source\Terminals.sln /m /p:configuration=DistributionRelease /p:Platform='Any CPU' /toolsversion:4.0 /t:rebuild" | Tee-Object $logFile -Append;
  
 .\PackOutput.ps1 | Tee-Object $logFile -Append;
 
