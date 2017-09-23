e626f58119a5c10e158b716e8f3ffc7197b6ae2b
Carl Hjerpe <lillecarl@gmail.com>
2017-09-23 14:46:58 +0200

    Implement MikroTik Winbox Plugin

---
+++ Source/Terminals.Plugins.WinBox/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Terminals.Plugins.WinBox")]
+[assembly: AssemblyDescription("WinBox")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("449dc14a-7c3f-4be5-ba39-4afd1c6304e6")]
+[assembly: InternalsVisibleTo("Tests")]
